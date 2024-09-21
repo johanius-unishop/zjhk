836086e8f487cf16a8ef5ad5b900fa075242bee6
Alex Gordi <gordi2001@mail.ru>
2024-09-21 09:22:26 +0300

    fix

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -26,10 +26,10 @@
                             <a class="nav-link" id="custom-tabs-four-profile-tab" data-toggle="pill" href="#custom-tabs-four-profile" role="tab" aria-controls="custom-tabs-four-profile" aria-selected="false">SEO</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="custom-tabs-four-messages-tab" data-toggle="pill" href="#custom-tabs-four-messages" role="tab" aria-controls="custom-tabs-four-messages" aria-selected="false">Изображения</a>
+                            <a class="nav-link" id="custom-tabs-four-messages-tab" data-toggle="pill" href="#custom-tabs-four-messages" role="tab" aria-controls="custom-tabs-four-messages" aria-selected="false">Фото</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="custom-tabs-four-files-tab" data-toggle="pill" href="#custom-tabs-four-files" role="tab" aria-controls="custom-tabs-four-files" aria-selected="false">Файлы для загрузки</a>
+                            <a class="nav-link" id="custom-tabs-four-files-tab" data-toggle="pill" href="#custom-tabs-four-files" role="tab" aria-controls="custom-tabs-four-files" aria-selected="false">Файлы</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" id="custom-tabs-four-settings-tab" data-toggle="pill" href="#custom-tabs-four-settings" role="tab" aria-controls="custom-tabs-four-settings" aria-selected="false">Характеристики</a>
@@ -37,7 +37,9 @@
                         <li class="nav-item">
                             <a class="nav-link" id="custom-tabs-four-analog-tab" data-toggle="pill" href="#custom-tabs-four-analog" role="tab" aria-controls="custom-tabs-four-analog" aria-selected="false">Аналоги</a>
                         </li>
-                        {{-- <li class="nav-item">
+                        <li class="nav-item">
+                            <a class="nav-link" id="custom-tabs-four-price-tab" data-toggle="pill" href="#custom-tabs-four-price" role="tab" aria-controls="custom-tabs-four-price" aria-selected="false">Торговля и цены</a>
+                        </li> {{-- <li class="nav-item">
                     <a class="nav-link" id="custom-tabs-five-settings-tab" data-toggle="pill"
                         href="#custom-tabs-five-settings" role="tab" aria-controls="custom-tabs-five-settings"
                         aria-selected="false">Видео </a>
@@ -179,33 +181,105 @@
                             <livewire:gallery :record="$product" />
 
                         </div>
-
                         <div class="tab-pane fade" id="custom-tabs-four-files" role="tabpanel" aria-labelledby="custom-tabs-four-files-tab">
                             <livewire:product-file-upload-component :record="@$product" />
                         </div>
                         <div class="tab-pane fade" id="custom-tabs-four-settings" role="tabpanel" aria-labelledby="custom-tabs-four-settings-tab">
-                            {{-- <livewire:product-variant-component :record="@$product" /> --}}
                             <livewire:test-component :record="@$product" />
-
                         </div>
-
                         <div class="tab-pane fade" id="custom-tabs-four-analog" role="tabpanel" aria-labelledby="custom-tabs-four-analog-tab">
-                            {{-- <livewire:product-variant-component :record="@$product" /> --}}
                             <livewire:analog-vendor-component :record="@$product" />
+                        </div>
+                        <div class="tab-pane fade" id="custom-tabs-four-price" role="tabpanel" aria-labelledby="custom-tabs-four-price-tab">
+                            Марккетплейсы, цены и прочее
 
+                            <div class="row">
+                                <div class="col-3">
+                                    <div class="form-group">
+                                        <label for="barcode">barcode </label>
+                                        <input type="text" class="form-control" name="barcode" value="{{ @$product->barcode }}">
+                                        @error('barcode')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tn_ved">tn_ved </label>
+                                        <input type="text" class="form-control" name="tn_ved" value="{{ @$product->tn_ved }}">
+                                        @error('tn_ved')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-3">
+
+                                    <div class="form-group">
+                                        <label for="supplier_price">supplier_price</label>
+                                        <input type="text" class="form-control" name="supplier_price" value="{{ @$product->supplier_price }}">
+                                        @error('supplier_price')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="moq_supplier">moq_supplier</label>
+                                        <input type="text" class="form-control" name="moq_supplier" value="{{ @$product->moq_supplier }}">
+                                        @error('moq_supplier')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+
+
+
+
+                                </div>
+
+                                <div class="col-3">
+                                </div>
+                                <div class="col-3">
+                                    <div class="form-group">
+                                        <label for="package_weight">Вес упаковки (единица измерения???)</label>
+                                        <input type="text" class="form-control" name="package_weight" value="{{ @$product->package_weight }}">
+                                        @error('package_weight')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="package_width">Ширина упаковки(единица измерения)</label>
+                                        <input type="text" class="form-control" name="package_width" value="{{ @$product->package_width }}">
+                                        @error('package_width')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="package_height">Высота упаковки(единица измерения)</label>
+                                        <input type="text" class="form-control" name="package_height" value="{{ @$product->package_height }}">
+                                        @error('package_height')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="package_length">Длина упаковки(единица измерения)</label>
+                                        <input type="text" class="form-control" name="package_length" value="{{ @$product->package_length }}">
+                                        @error('package_length')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class=" py-3 form-row justify-content-center">
-                    <a class="btn .btn-lg btn-success " href="{{ route('admin.product.index') }}" role="button"> <i class="fa fa-arrow-left "></i> К списку</a>&nbsp;
-
-                    <button type="submit" name="action" value="save" class="btn btn-primary">Сохранить</button>
-                    &nbsp;
-                    <button type="submit" name="action" value="save-exit" class="btn btn-primary">Сохранить и
-                        закрыть</button>
-                </div>
+            </div>
+            <div class=" py-3 form-row justify-content-center">
+                <a class="btn .btn-lg btn-success " href="{{ route('admin.product.index') }}" role="button"> <i class="fa fa-arrow-left "></i> К списку</a>&nbsp;
+                <button type="submit" name="action" value="save" class="btn btn-primary">Сохранить</button>
+                &nbsp;
+                <button type="submit" name="action" value="save-exit" class="btn btn-primary">Сохранить и закрыть</button>
             </div>
         </div>
+    </div>
 
     </div>
 
