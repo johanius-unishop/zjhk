113e8d42530cd64181864bb1d743bb90c08c1df2
Alex Gordi <gordi2001@mail.ru>
2024-10-04 12:55:22 +0300

    user_groups

--- database/migrations/2024_01_17_124501_create_user_groups_table.php
+++ database/migrations/2024_01_17_124501_create_user_groups_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('user_groups');
         Schema::create('user_groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
