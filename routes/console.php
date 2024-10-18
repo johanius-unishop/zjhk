4bcd921fbcc5384c427d55750fa08e4b063ac1fd
johanius-unishop <61455575+johanius-unishop@users.noreply.github.com>
2024-10-18 12:07:20 +0300

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('fetch:exchange-rates')->dailyAt('05:00')->timezone('Europe/Moscow');
+Schedule::command('fetch:exchange-rates')->dailyAt('12:09')->timezone('Europe/Moscow');
 Schedule::command('products:update-stock')->hourly();
 Schedule::command('products:updateCompositePrice')->dailyAt('00:00')->timezone('Europe/Moscow');
 Schedule::command('products:updateCompositePrice')->dailyAt('12:00')->timezone('Europe/Moscow');
