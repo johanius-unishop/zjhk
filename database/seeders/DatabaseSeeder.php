39f59806601b3e82a8fca71392727fd2235a05b2
johanius <info@kevtek.ru>
2025-01-25 17:12:37 +0300

    seeders edit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,18 +24,15 @@ class DatabaseSeeder extends Seeder
         $this->call(VendorsTableSeeder::class);
         $this->call(AnalogVendorsTableSeeder::class);
         $this->call(DeliveryMethodsTableSeeder::class);
-        // $this->call(ProductsOldTableSeeder::class);
         $this->call(AnalogiesTableSeeder::class);
         $this->call(RelatedProductTypesTableSeeder::class);
-        $this->call(PropertiesTableSeeder::class);
         $this->call(PropertyValuesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
-        $this->call(ProductCompositeElementsTableSeeder::class);
+        
         $this->call(ProductTypeCompositeElementsTableSeeder::class);
-        $this->call(ProductPdfsTableSeeder::class);
-        $this->call(ProductImagesTableSeeder::class);
+        
         $this->call(FaqsTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
-        // $this->call(AdminsTableSeeder::class);
+        
     }
 }
