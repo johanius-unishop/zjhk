333616aa88bede26fdd2d8bb9142d1207c397c8f
johanius <info@kevtek.ru>
2025-01-25 12:44:40 +0300

    translate mail

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword as NotificationsResetPassword;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPassword extends NotificationsResetPassword
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param  string  $token
+     */
+    public function __construct($token)
+    {
+        parent::__construct($token);
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Уведомление о сбросе пароля - ' . config('app.name'))
+                    ->line('Вы получаете это письмо, потому что мы получили запрос на сброс пароля для вашей учетной записи.')
+                    ->action('Сбросить пароль', url(config('app.url').route('password.reset', ['token' => $this->token], false)))
+                    ->line('Если вы не запрашивали сброс пароля, никаких дальнейших действий не требуется.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [];
+    }
+}
