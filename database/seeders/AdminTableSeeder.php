f98c1ff79f57ce75fa8d530ec1f2929b063fc583
johanius <info@kevtek.ru>
2025-01-25 16:52:25 +0300

    seeders edit

--- database/seeders/AdminTableSeeder.php
+++ database/seeders/AdminTableSeeder.php
@@ -20,16 +20,11 @@ class AdminTableSeeder extends Seeder
      */
     public function run(): void
     {
-        // $faker = Faker::create('ru_RU');
         Artisan::call('cache:clear');
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-        // $all_roles_in_database = Role::all()->pluck('name');
 
         $user = Admin::create([
             'name' => 'Иван',
-
-            'last_name' => 'Иванов',
-            'patronymic' => 'Иванович',
             'email' => 'admin@admin.com',
             'password' => '$2y$10$GuaoFnDw17nZcbNNDq6aO.1wZHpLr3hGyADjN14J0kj1yjrPxdHJm',                // 'password'
             'phone_number' => '790231323123',
@@ -42,13 +37,9 @@ class AdminTableSeeder extends Seeder
         Admin::find(1)->assignRole('super-admin');
 
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-        // $all_roles_in_database = Role::all()->pluck('name');
 
         $user = Admin::create([
             'name' => 'gordi2001@mail.ru',
-
-            'last_name' => 'gordi2001@mail.ru',
-            'patronymic' => 'gordi2001@mail.ru',
             'email' => 'gordi2001@mail.ru',
             'password' => Hash::make('gordi2001@mail.ru'),
             'phone_number' => '790231323123',
@@ -59,57 +50,5 @@ class AdminTableSeeder extends Seeder
         Artisan::call('cache:clear');
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
         Admin::find(2)->assignRole('super-admin');
-
-        // for ($i = 1; $i < 6; $i++) {
-
-        //     $user = Admin::create([
-        //         'name' => $faker->firstName,
-
-        //         'last_name' => $faker->lastName,
-        //         'patronymic' => $faker->middleName,
-        //         'email' => 'user' . $i . '@admin.com',
-        //         'password' => '$2y$10$GuaoFnDw17nZcbNNDq6aO.1wZHpLr3hGyADjN14J0kj1yjrPxdHJm',                // 'password'
-        //         'phone_number' => $faker->numerify('+79#########'),
-        //         'is_admin' => '0',
-        //         'created_at' => now(),
-        //         'updated_at' => now(),
-        //     ]);
-        //     $user->assignRole('content-editor');
-
-        // }
-
-
-        // for ($i = 7; $i < 10; $i++) {
-
-        //     $user = Admin::create([
-        //         'name' => $faker->firstName,
-        //         'last_name' => $faker->lastName,
-        //         'patronymic' => $faker->middleName,
-        //         'email' => 'user' . $i . '@admin.com',
-        //         'password' => '$2y$10$GuaoFnDw17nZcbNNDq6aO.1wZHpLr3hGyADjN14J0kj1yjrPxdHJm',                // 'password'
-        //         'phone_number' => $faker->numerify('+79#########'),
-        //         'is_admin' => '0',
-        //         'created_at' => now(),
-        //         'updated_at' => now(),
-        //     ]);
-        //     $user->assignRole('product-editor');
-
-        // }
-        // for ($i = 11; $i < 13; $i++) {
-
-        //     $user = Admin::create([
-        //         'name' => $faker->firstName,
-        //         'last_name' => $faker->lastName,
-        //         'patronymic' => $faker->middleName,
-        //         'email' => 'user' . $i . '@admin.com',
-        //         'password' => '$2y$10$GuaoFnDw17nZcbNNDq6aO.1wZHpLr3hGyADjN14J0kj1yjrPxdHJm',                // 'password'
-        //         'phone_number' => $faker->numerify('+79#########'),
-        //         'is_admin' => '0',
-        //         'created_at' => now(),
-        //         'updated_at' => now(),
-        //     ]);
-        //     $user->assignRole('image-editor');
-
-        // }
     }
 }
