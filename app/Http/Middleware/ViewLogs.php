f3253b1f127ee3abb6e72d670f68a25d580ccb3a
johanius <info@kevtek.ru>
2025-01-28 23:28:45 +0300

    log

--- app/Http/Middleware/ViewLogs.php
+++ app/Http/Middleware/ViewLogs.php
@@ -5,15 +5,19 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class ViewLogs
 {
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->hasPermissionTo('view-logs')) {
-            return $next($request);
+        
+        if (!Gate::allows('manage content')) {
+            return abort(401);
         }
+        
 
-        abort(401, 'Unauthorised');
+        // Если все условия выполнены, продолжаем выполнение запроса
+        return $next($request);
     }
 }
