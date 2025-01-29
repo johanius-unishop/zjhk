e403331903d83e8be2932e0449702c298aca7365
johanius <info@kevtek.ru>
2025-01-29 15:26:49 +0300

    order fix

--- app/Livewire/OrderComponent.php
+++ app/Livewire/OrderComponent.php
@@ -3,7 +3,6 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use Illuminate\Support\Facades\Storage;
 use Livewire\WithFileUploads;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use App\Models\Order;
@@ -11,7 +10,7 @@ use App\Models\OrderComposition;
 use App\Models\Product;
 use App\Models\AdditionalSetting;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
-use Carbon\Carbon;
+
 
 class OrderComponent extends Component
 {
@@ -45,6 +44,7 @@ class OrderComponent extends Component
         $orderNameCell = $settings['orderNameCell'];
         $orderDateCell = $settings['orderDateCell'];
         $startRow = intval($settings['startRowData']); // Преобразуем значение startRowData в целое число
+        $vendorCol = $settings['vendorColumn'];
         $modelCol = $settings['modelColumn'];
         $quantityCol = $settings['quantityColumn'];
         
@@ -60,33 +60,43 @@ class OrderComponent extends Component
         $orderNumber = trim($worksheet->getCell($orderNameCell)->getValue());
         $orderDate = trim($worksheet->getCell($orderDateCell)->getValue());
         
-                if (!Order::where('order_number', $orderNumber)->first()) {
-                    $row = $startRow;
-                    $missingProducts = []; // Массив для хранения недостающих товаров
+        if (!Order::where('order_number', $orderNumber)->where('order_date', $orderDate)->exists()) {
+            $row = $startRow;
+            $missingProducts = []; // Массив для хранения недостающих товаров
 
-                    // Проверяем все товары перед загрузкой
-                    while ($worksheet->getCell($modelCol . $row)->getValue() !== null) {
-                        $productModel = $worksheet->getCell($modelCol . $row)->getValue();
-                        $product = Product::where('name', $productModel)->first();
+            // Проверяем все товары перед загрузкой
+            while ($worksheet->getCell($modelCol . $row)->getValue() !== null) {
+                $productVendor = $worksheet->getCell($vendorCol . $row)->getValue();
+                $productModel = $worksheet->getCell($modelCol . $row)->getValue();
+                $product = Product::where('name', $productModel)
+                    ->where(function($query) use ($productVendor) {
+                        $query->whereHas('vendor', function ($subQuery) use ($productVendor) {
+                            $subQuery->where('name', $productVendor);
+                        })->orWhereHas('vendor', function ($subQuery) use ($productVendor) {
+                            $subQuery->where('short_name', $productVendor);
+                        });
+                    })
+                    ->first();
 
-                        if (!$product) {
-                            $missingProducts[] = $productModel;
-                        }
-
-                        $row++;
-                    }
+                if (!$product) {
+                    $missingProducts[] = $productModel;
                 }
-                
-                if (!empty($missingProducts)) {
 
-                    session()->flash('order_error_message', 'В файле заказа есть товары, которых нет в БД:');
+                $row++;
+            }
+        
+                
+        if (!empty($missingProducts)) {
+
+            session()->flash('order_error_message', 'В файле заказа есть товары, которых нет в БД:');
                     session()->flash('missingProducts', $missingProducts);
                 
                     return redirect()->back();
+                    $this->reset('xls_file');
                 }
                 $order = new Order();
                 $order->order_number = $orderNumber;
-                $order->order_date = $orderDate; // Текущая дата в формате дд.мм.гггг
+                $order->order_date = $orderDate;
                 $order->received = 0;
                 
                 $order->save();
@@ -96,10 +106,19 @@ class OrderComponent extends Component
 
                 $row = $startRow;
                     while ($worksheet->getCell($modelCol . $row)->getValue() !== null) {
+                        $productVendor = $worksheet->getCell($vendorCol . $row)->getValue();
                         $productModel = $worksheet->getCell($modelCol . $row)->getValue();
                         $quantity = $worksheet->getCell($quantityCol . $row)->getValue();
 
-                        $product = Product::where('name', $productModel)->first();
+                        $product = Product::where('name', $productModel)
+                            ->where(function($query) use ($productVendor) {
+                                $query->whereHas('vendor', function ($subQuery) use ($productVendor) {
+                                    $subQuery->where('name', $productVendor);
+                                })->orWhereHas('vendor', function ($subQuery) use ($productVendor) {
+                                    $subQuery->where('short_name', $productVendor);
+                                });
+                            })
+                            ->first();
 
                         $orderComposition = new OrderComposition();
                         $orderComposition->quantity = $quantity;
@@ -112,5 +131,11 @@ class OrderComponent extends Component
                 $this->reset('xls_file');
                 $this->dispatch('update-order-table');       
                 return;    
+        }
+        else
+        {
+            session()->flash('order_error_message', 'Заказ с таким номером и такой датой уже существует!');
+            return redirect()->back();
+        }
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,6 +10,8 @@ class Order extends Model
     use HasFactory;
 
     protected $fillable = [
+        'order_number',
+        'order_date',
         'received'
     ];
 
