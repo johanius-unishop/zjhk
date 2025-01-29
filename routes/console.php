da48350c26ec79a2c95fb1ca7a8db39691c33e12
johanius <info@kevtek.ru>
2025-01-29 20:54:49 +0300

    run queue:work with param --stop-when-empty

--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,9 @@
 <?php
+
+use Illuminate\Support\Facades\Schedule;
+
+
+
+Schedule::command('fetch:exchange-rates')->dailyAt('05:00')->timezone('Europe/Moscow');
+Schedule::command('products:update-simple-products-stock-from-1c')->everyFiveMinutes();
+Schedule::command('queue:work --stop-when-empty --queue=high,default')->everyMinute();
