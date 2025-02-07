113c48a083f2159f03d2e5a25800fd4cb61b4c38
johanius <info@kevtek.ru>
2025-02-07 21:12:10 +0300

    add import property table fix

--- app/Http/Controllers/Admin/ImportController.php
+++ app/Http/Controllers/Admin/ImportController.php
@@ -275,8 +275,6 @@ class ImportController extends Controller
         
                 // Определяем диапазон ячеек на втором листе, который содержит допустимые значения
                 $sourceCellRange = $sourceColumnLetter . '$1' . ':' . $sourceColumnLetter . '$' . (count($variants_value[$property->id])); // Диапазон начинается со второй строки и до последней строки, где были записаны значения
-                Log::info('Диапазон данных:' . $cellRange);
-                Log::info('Источник вариантов:' . $sourceCellRange);
                 // Создаем выпадающий список, используя ссылку на диапазон ячеек на втором листе
                 $validation = $sheet->getDataValidation($cellRange)
                                     ->setType(DataValidation::TYPE_LIST)
