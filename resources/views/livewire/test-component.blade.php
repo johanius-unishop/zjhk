479f423ae370e7cc84f46b137e95521e406af11e
johanius <info@kevtek.ru>
2025-02-21 11:42:48 +0300

    product edit product_name

--- resources/views/livewire/test-component.blade.php
+++ resources/views/livewire/test-component.blade.php
@@ -1,14 +1,17 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
     <div class="row">
         <div class="col-12">
-            <div class=" py-3 form-row justify-content-left">{{ $productType->name }}: &nbsp;<b> {{ $product->name }} </b></div>
+            <div class="py-3 form-row justify-content-left">
+                @if($productType->name)
+                    {{ $productType->name }}:  <b>{{ $product->name }}</b>
+                @else
+                    Товару не назначен Тип продукта
+                @endif
+            </div>
             <table class="table table-striped">
                 <thead>
-                    
                     <th>Характеристика</th>
                     <th>Значение</th>
-                    
                 </thead>
                 @foreach ($productType->properties as $property)
                 <tr wire:key="property_{{ $property->id }}">
@@ -28,10 +31,9 @@
                 </tr>
                 @endforeach
             </table>
-            <div class=" py-3 form-row justify-content-center">
-                <button wire:click="save" type="button" class="btn btn-primary" {{ $isDisabled==true ? 'disabled' : ''
-                }}>Сохранить </button>
+            <div class="py-3 form-row justify-content-center">
+                <button wire:click="save" type="button" class="btn btn-primary" {{ $isDisabled==true ? 'disabled' : '' }}>Сохранить</button>
             </div>
         </div>
-    </div>   
+    </div>
 </div>
