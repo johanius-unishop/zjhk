d168328d50ad5bd8959e0891fca7f3fce1c643d1
johanius <info@kevtek.ru>
2025-08-02 19:34:07 +0300

    fix mobile header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -451,10 +451,7 @@
                         <img src="images/icons/phone-stroke.svg" alt="телефон">
                         <span>+7 (812) 200 82 75</span>
                      </a>
-                     <a class="header-mobile__phone-link _last" href="tel:+78122008275">
-                        <img src="images/icons/phone-stroke.svg" alt="телефон">
-                        <span>+7 (812) 200 82 75</span>
-                     </a>
+                     
                      <a class="header-mobile__email-wrapper" href="mailto:info@kevtek.ru">
                         <img src="images/icons/envelope-stroke.svg" alt="почта">
                         <span>info@kevtek.ru</span>
