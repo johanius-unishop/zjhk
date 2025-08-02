04557f8995a877ac1e68601bb442e3e60f99c317
johanius <info@kevtek.ru>
2025-08-02 17:18:06 +0300

    mobile header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -442,3 +442,101 @@
       </div>
    </div>
 </header>
+<div id="header-mobile" class="header-mobile">
+         <div class="header-mobile__top">
+            <div class="header__container container">
+               <div class="header-mobile__top-wrapper">
+                  <div class="header-mobile__contacts">
+                     <a class="header-mobile__phone-link" href="tel:+78122008275">
+                        <img src="img/icons/phone-stroke.svg" alt="телефон">
+                        <span>+7 (812) 200 82 75</span>
+                     </a>
+                     <a class="header-mobile__phone-link _last" href="tel:+78122008275">
+                        <img src="img/icons/phone-stroke.svg" alt="телефон">
+                        <span>+7 (812) 200 82 75</span>
+                     </a>
+                     <a class="header-mobile__email-wrapper" href="mailto:info@kevtek.ru">
+                        <img src="img/icons/envelope-stroke.svg" alt="почта">
+                        <span>info@kevtek.ru</span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="header-mobile__bottom">
+            <div class="header-mobile__container container">
+               <div class="header-mobile__bottom-wrapper">
+                  <a href="#" class="header-mobile__logo-link">
+                     <img src="img/icons/logo.svg" alt="логотип" class="header-mobile__logo">
+                  </a>
+                  <div class="header-mobile__search-wrapper">
+                     <a href="#" class="header-mobile__catalogue-btn btn">
+                        <img src="img/icons/catalog.svg" alt="каталог">
+                     </a>
+                     <div class="header-mobile__search-field">
+                        <input type="mobile-search" name="mobile-search" id="mobile-search" placeholder="Поиск по каталогу"
+                           autocomplete="off" class="header-mobile__search-input">
+                        <button class="header-mobile__search-submit">
+                           <img src="img/icons/zoom-glass.svg" alt="поиск">
+                        </button>
+                        <div id="header-mobile__result" class="header-mobile__result" style="display: none">
+                           <ul class="header-mobile__result-wrapper">
+                              <li>
+                                 <a href="#">
+                                    <p>
+                                       <img src="img/products/example.png" alt="пример товара">
+                                       <span>HA-003-F</span>
+                                       <span>09200032711</span>
+                                    </p>
+                                    <span>380 ₽</span>
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#">
+                                    <p>
+                                       <img src="img/products/example.png" alt="пример товара">
+                                       <span>HA-003-F</span>
+                                       <span>09200032711</span>
+                                    </p>
+                                    <span>380 ₽</span>
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#">
+                                    <p>
+                                       <img src="img/products/example.png" alt="пример товара">
+                                       <span>HA-003-F</span>
+                                       <span>09200032711</span>
+                                    </p>
+                                    <span>380 ₽</span>
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#">
+                                    <p>
+                                       <img src="img/products/example.png" alt="пример товара">
+                                       <span>HA-003-F</span>
+                                       <span>09200032711</span>
+                                    </p>
+                                    <span>380 ₽</span>
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#">
+                                    <p>
+                                       <img src="img/products/example.png" alt="пример товара">
+                                       <span>HA-003-F</span>
+                                       <span>09200032711</span>
+                                    </p>
+                                    <span>380 ₽</span>
+                                 </a>
+                              </li>
+                           </ul>
+                           <a class="header-mobile__result-btn" href="#">Посмотреть все товары <span>(115)</span></a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
