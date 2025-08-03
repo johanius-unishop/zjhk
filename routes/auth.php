7dbb6c945348c6b4bcb2903b78e457e38b2aa0df
johanius <info@kevtek.ru>
2025-08-03 12:20:00 +0300

    modal Login

--- routes/auth.php
+++ routes/auth.php
@@ -13,10 +13,27 @@ use Illuminate\Support\Facades\Route;
 
 // Очистим маршруты авторизации и оставим только обработчики POST-запросов
 Route::middleware('guest')->group(function () {
-    Route::post('register', [RegisteredUserController::class, 'store'])->name('register');
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+    Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+                ->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create']) 
+                ->name('password.request');
+
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+                ->name('password.email');
+
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+                ->name('password.reset');
+
+    Route::post('reset-password', [NewPasswordController::class, 'store'])
+                ->name('password.store');
 });
 
 Route::middleware('auth')->group(function () {
