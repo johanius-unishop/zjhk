4d74587baeb0b5cf8fea94fc6fc5ef10d917107f
johanius <info@kevtek.ru>
2025-08-04 18:38:21 +0300

    add news page

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,14 +1,12 @@
-@Breadcrumbs::generate()
-@if(Breadcrumbs::exists())
-    <div class="breadcrumbs">
-        <ul class="breadcrumbs__list">
-            @foreach(Breadcrumbs::generate() as $crumb)
-                @if(!$loop->last)
-                    <li><a href="{{ $crumb->url }}">{{ $crumb->title }}</a><img src="/images/icons/arrow-right.svg" alt="стрелка"></li>
-                @else
-                    <li><span>{{ $crumb->title }}</span></li>
-                @endif
-            @endforeach
-        </ul>
-    </div>
-@endif
+<!-- Вывести хлебные крошки -->
+<div class="breadcrumbs">
+    <ul class="breadcrumbs__list">
+        @foreach(Breadcrumbs::current() as $crumb)
+            @if(!$loop->last)
+                <li><a href="{{ $crumb->url }}">{{ $crumb->title }}</a><img src="/images/icons/arrow-right.svg" alt="стрелка"></li>
+            @else
+                <li><span>{{ $crumb->title }}</span></li>
+            @endif
+        @endforeach
+    </ul>
+</div>
