1485a0d31a343e4ed9ead92f9ec2ce97e001ecf5
johanius <info@kevtek.ru>
2025-08-04 20:19:20 +0300

    add articles page

--- resources/views/components/main-page/articles.blade.php
+++ resources/views/components/main-page/articles.blade.php
@@ -2,7 +2,7 @@
     <div class="articles__container container">
         <div class="articles__title-container">
             <h2 class="articles__title">Статьи</h2>
-            <button class="title-btn btn">Показать все</button>
+            <button onclick="window.location='{{ route('article.index') }}'" class="title-btn btn">Показать все</button>
             <a href="{{ route('article.index') }}" class="title-btn btn">Показать все</a>
         </div>
         <div class="articles__body">
