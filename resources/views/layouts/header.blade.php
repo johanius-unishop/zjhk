cf3d3babe3707c782b300766362c6e83685e613d
johanius <info@kevtek.ru>
2025-08-04 14:17:05 +0300

    divide header and footer on components

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header id="header" class="header">
    @include('components.header.header-top')
-   @include('components.header.headr-bottom')
+   @include('components.header.header-bottom')
 </header>
 <div id="header-mobile" class="header-mobile">
    @include('components.header.header-mobile-top')
