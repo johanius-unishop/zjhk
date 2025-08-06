20375f8bb2d63569a9e9d219b350d74e83280163
johanius <info@kevtek.ru>
2025-08-06 15:57:26 +0300

    add product page

--- resources/views/components/product-page/price.blade.php
+++ resources/views/components/product-page/price.blade.php
@@ -1,7 +1,7 @@
 @php
     $formattedPrice =
-        floor($data['product']->price) === $product->price
-            ? number_format($product->price, 0, '', ' ')
-            : number_format($product->price, 2, '.', ' ');
+        floor($data['product']->price) === $data['product']>price
+            ? number_format($data['product']>price, 0, '', ' ')
+            : number_format($data['product']->price, 2, '.', ' ');
 @endphp
 <p>{{ $formattedPrice }} ₽</p>
