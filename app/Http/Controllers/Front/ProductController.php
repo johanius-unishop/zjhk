1e4c7f52568402e8794d796316aca9d73e57fb08
johanius <info@kevtek.ru>
2025-08-06 21:29:42 +0300

    add product page

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -33,7 +33,8 @@ class ProductController extends Controller
         // /$viewModel = new ProductViewModel($product);
         $images      = $product->getMedia('images');
         $parents = Category::ancestorsAndSelf($product->category_id)->toArray();
-
+        $characteristics = $product->getProperties();
+        dd($characteristics);
 
         $data = [
 
