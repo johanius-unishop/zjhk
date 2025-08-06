d67d2a70e4d21f396496b4e1f39d86fa116f36eb
johanius <info@kevtek.ru>
2025-08-06 11:33:21 +0300

    add product page

--- resources/views/components/product-page/breadcrumbs.blade.php
+++ resources/views/components/product-page/breadcrumbs.blade.php
@@ -1,9 +1,8 @@
 @if (Breadcrumbs::exists())
     <div class="breadcrumbs">
         <ul class="breadcrumbs__list">
-            @foreach (Breadcrumbs::generate('product.show', ['parents' => parents], product) as crumb)
-                @if (!crumb)
-                    @if (!loop->last)
+            @foreach (Breadcrumbs::generate('product.show', ['parents' => $parents], $product) as $crumb)
+                    @if (!$loop->last)
                         <li><a href="{{ $crumb->url }}">{{ $crumb->title }}</a><img src="/images/icons/arrow-right.svg"
                                 alt="стрелка"></li>
                     @else
