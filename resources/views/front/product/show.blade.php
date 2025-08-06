05a3fcc54f47672a22592b3dc1c70257652d9edc
johanius <info@kevtek.ru>
2025-08-06 14:42:36 +0300

    add product page

--- resources/views/front/product/show.blade.php
+++ resources/views/front/product/show.blade.php
@@ -116,7 +116,7 @@
                                             </div>
                                             <div class="product-desc__short">
                                                 <p>Описание:</p>
-                                                <p>{{$data['product']->short_description }}/p>
+                                                <p>{{$data['product']->short_description }}</p>
                                             </div>
                                             <div class="product-desc__prop-link">
                                                 <a href="#">Все характеристики</a>
