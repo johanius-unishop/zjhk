a56ae57cbdc9b096a5bf2aa2a256c2237a16413e
johanius <info@kevtek.ru>
2025-08-07 16:28:22 +0300

    add product page

--- resources/views/components/product-page/composition-set.blade.php
+++ resources/views/components/product-page/composition-set.blade.php
@@ -2,7 +2,7 @@
     <h5 class="product-desc__complectation-title">Комплектация</h5>
     <div class="product-desc__complectation-wrapper">
         @foreach ($data['compositeSet'] as $element)
-        <p>{{ $element['type'] }} {{ $element->['name'] }} {{ $element->['article'] }} - {{ $element->['qty'] }} шт.</p>
+            <p>{{ $element['type'] }} {{ $element['model'] }} {{ $element['article'] }} - {{ $element['qty'] }} шт.</p>
         @endforeach
     </div>
 </div>
