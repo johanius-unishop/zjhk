2ae019b55634a915ed335824fea08b4b8a984093
johanius <info@kevtek.ru>
2025-08-07 21:20:47 +0300

    add product page

--- resources/views/components/product-page/composition-set.blade.php
+++ resources/views/components/product-page/composition-set.blade.php
@@ -1,8 +1,28 @@
-<div class="product-desc__complectation">
-    <h5 class="product-desc__complectation-title">Комплектация</h5>
-    <div class="product-desc__complectation-wrapper">
-        @foreach ($data['compositionSet'] as $element)
-            <p>{{ $element['type'] }}: <a href="{{ $element['link'] }}">{{ $element['model'] }}</a>  ({{ $element['article'] }}) - {{ $element['qty'] }} шт.</p>
-        @endforeach
+@if (!empty($data['compositionSet']))
+    <div class="product-desc__complectation">
+        <h5 class="product-desc__complectation-title">Комплектация</h5>
+        <div class="product-desc__complectation-wrapper">
+            @foreach ($data['compositionSet'] as $element)
+                @if ($element['qty'] > 1)
+                    <p>{{ $element['type'] }}:
+                       <a href="{{ $element['link'] }}">
+                           {{ $element['model'] }}
+                           @if ($element['article'] !== '' && $element['article'] != $element['model'])
+                               ({{ $element['article'] }})
+                           @endif
+                       </a> - {{ $element['qty'] }} шт.
+                    </p>
+                @else
+                    <p>{{ $element['type'] }}:
+                       <a href="{{ $element['link'] }}">
+                           {{ $element['model'] }}
+                           @if ($element['article'] !== '' && $element['article'] != $element['model'])
+                               ({{ $element['article'] }})
+                           @endif
+                       </a>
+                    </p>
+                @endif
+            @endforeach
+        </div>
     </div>
-</div>
+@endif
