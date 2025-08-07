b54cbc4742fecc3d780870b949c5459e2136799a
johanius <info@kevtek.ru>
2025-08-07 12:33:05 +0300

    add product page

--- resources/views/components/product-page/brand-section.blade.php
+++ resources/views/components/product-page/brand-section.blade.php
@@ -1,13 +1,17 @@
 <div class="product-desc__brand">
-    <h3>{{ $data['product']->vendor->short_name }} <img src="/images/icons/attention-gray.svg" alt="логотип"></h3>
-    <a href="#">Все товары бренда</a>
-    <img class="product-desc__logo" src="/images/icons/partner-logo.png" alt="логотип">
+    @if($data['product']->vendor)
+        <h3>{{ $data['product']->vendor->short_name }} <img src="/images/icons/attention-gray.svg" alt="логотип"></h3>
+        <a href="#">Все товары бренда</a>
+        <img class="product-desc__logo" src="/images/icons/partner-logo.png" alt="логотип">
+    @endif
 </div>
 <div class="product-desc__country">
-    <p><img src="/images/icons/china_flag.svg" alt="страна"> <span>Страна производства:
-            {{ $data['product']->vendor->country->name }}</span>
-    </p>
-    <p><img src="/images/icons/protect.svg" alt="гарантия"> <span>Гарантия производителя:
-            {{ $data['product']->vendor->warranty }}</span>
-    </p>
+    @if($data['product']->vendor && $data['product']->vendor->country)
+        <p><img src="/images/icons/china_flag.svg" alt="страна"> <span>Страна производства:
+                {{ $data['product']->vendor->country->name }}</span></p>
+    @endif
+    @if($data['product']->vendor && isset($data['product']->vendor->warranty))
+        <p><img src="/images/icons/protect.svg" alt="гарантия"> <span>Гарантия производителя:
+                {{ $data['product']->vendor->warranty }}</span></p>
+    @endif
 </div>
