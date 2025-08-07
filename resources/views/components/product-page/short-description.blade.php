f55782413ff2d22d31bfedef76778708cd8db0ed
johanius <info@kevtek.ru>
2025-08-07 13:48:22 +0300

    add product page

--- resources/views/components/product-page/short-description.blade.php
+++ resources/views/components/product-page/short-description.blade.php
@@ -1,6 +1,6 @@
 <div class="product-desc__short">
     @if ($data['product']->short_description && trim($data['product']->short_description))
         <p>Описание:</p>
-        <p>{{ $data['product']->short_description }}</p>
+        <p>{{ $data['product']->short_description }} </p>
     @endif
 </div>
