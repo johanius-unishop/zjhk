2cc507037e4ed064a58eb6c8aab0fd46e5a8367e
johanius <info@kevtek.ru>
2025-08-08 13:44:35 +0300

    add product page

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -132,7 +132,7 @@ class ProductController extends Controller
 
 
 
-        dd($product->seo->title);
+        dd(SEOMeta::getTitle());
 
         SEOMeta::setTitle($product->seo->title);
         SEOMeta::setDescription($product->seo->description);
