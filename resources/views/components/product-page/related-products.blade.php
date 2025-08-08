99d5da0fa564330abd7c443d4ddc7d655ee60709
johanius <info@kevtek.ru>
2025-08-08 20:39:26 +0300

    add product page

--- resources/views/components/product-page/related-products.blade.php
+++ resources/views/components/product-page/related-products.blade.php
@@ -7,7 +7,6 @@
                 </h2>
                 <div class="opt__body">
                     <div class="swiper opt-slider">
-                        @dd($related_item['relatedProducts']);
                         <div class="swiper-wrapper">
                             @foreach ($related_item['relatedProducts'] as $related_product_item)
                                 <div class="swiper-slide product-page__item">
@@ -15,9 +14,9 @@
                                         <div>
                                             <div class="swiper product-page-slider">
                                                 <div class="swiper-wrapper">
-                                                    @foreach ($related_product_item->getMedia('images') as $mediaItem)
+                                                    @foreach ($related_product_item->product->getMedia('images') as $mediaItem)
                                                     <div class="swiper-slide"> <img
-                                                            src="{{ $mediaItem->getUrl() }}" alt="Изображение {{ $related_product_item->name }}">
+                                                            src="{{ $mediaItem->getUrl() }}" alt="Изображение {{ $related_product_item->product->name }}">
                                                     </div>
                                                     @endforeach
                                                 </div>
@@ -29,18 +28,18 @@
                                             </button>
                                         </div>
                                         <div data-layout class="product-page__title-container card-layout">
-                                            <h5>{{ $related_product_item->name }}</h5>
-                                            <p>{{ $related_product_item->article }}</p>
+                                            <h5>{{ $related_product_item->product->name }}</h5>
+                                            <p>{{ $related_product_item->product->article }}</p>
                                             <div>
                                                 <img src="{{ asset('images/icons/star.svg') }}" alt="рейтинг"><span>4.9</span>
                                                 <a href="#"><span>152 </span>отзыва</a>
                                             </div>
                                         </div>
                                         <div data-layout class="product-page__info-container card-layout">
-                                            <p>{{ $related_product_item->price }} ₽</p>
-                                            @if ($related_product_item->stock > 0)
-                                                <p>В наличии {{ $related_product_item->stock }} шт.</p>
-                                            @elseif ($related_product_item->stock == 0)
+                                            <p>{{ $related_product_item->product->price }} ₽</p>
+                                            @if ($related_product_item->product->stock > 0)
+                                                <p>В наличии {{ $related_product_item->product->stock }} шт.</p>
+                                            @elseif ($related_product_item->product->stock == 0)
                                                 <p>Нет в наличии.</p>
                                             @else
                                                 <p>Ожидается поступление</p>
