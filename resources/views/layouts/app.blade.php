d191c56002ce0131719c63a27408eac516c84355
johanius <info@kevtek.ru>
2025-08-08 13:11:55 +0300

    add product page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title', config('app.name', 'Laravel'))</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="body">
-        <div class="app__wrapper">
-            @include('layouts.header')
+    <title>{{ SEOMeta::getTitle() }}</title>
+    <meta name="description" content="{{ SEOMeta::getDescription() }}">
+    <meta name="keywords" content="{{ implode(', ', SEOMeta::getKeywords()) }}">
 
-            @yield ('content')
-            @include('layouts.footer')
-            @include('partials.login')
-            <div id="scroll" class="scroll">
-                <span>+</span>
-            </div>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="body">
+    <div class="app__wrapper">
+        @include('layouts.header')
+
+        @yield ('content')
+        @include('layouts.footer')
+        @include('partials.login')
+        <div id="scroll" class="scroll">
+            <span>+</span>
+        </div>
 
 
-    @livewireScripts
-    @yield('scripts')
-    </body>
+        @livewireScripts
+        @yield('scripts')
+</body>
+
 </html>
