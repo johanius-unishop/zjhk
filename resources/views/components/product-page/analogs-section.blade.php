26e9f09da6e17c1b9b325e5b2951b7a4624f0955
johanius <info@kevtek.ru>
2025-08-10 15:21:50 +0300

    Analogs section

--- resources/views/components/product-page/analogs-section.blade.php
+++ resources/views/components/product-page/analogs-section.blade.php
@@ -3,13 +3,13 @@
         <h2 class="product-desc__analog-title">Аналоги</h2>
         <div class="product-desc__analog-invite">
             @auth
-            <p>Показываем информацию об аналогах</p>
+                <p>Показываем информацию об аналогах</p>
             @else
-            <img src="{{ asset('images/icons/attention.svg') }}" alt="войдите">
-            <p class="product-desc__analog-text">
-                <button data-modal-link="account"><span>Войдите</span>,</button>
-                чтобы увидеть информацию по аналогам к данному товару.
-            </p>
+                <img src="{{ asset('images/icons/attention.svg') }}" alt="войдите">
+                <p class="product-desc__analog-text">
+                    <button data-modal-link="account"><span>Войдите</span>,</button>
+                    чтобы увидеть информацию по аналогам к данному товару.
+                </p>
             @endauth
         </div>
         @include('components.product-page.reviews-and-questions-section')
