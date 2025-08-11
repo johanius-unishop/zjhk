abb7407ce6bb2a9c28e7f31d538e29bcbc3e1504
johanius <info@kevtek.ru>
2025-08-11 20:55:33 +0300

    laravel 12.0 version

--- resources/views/components/product-page/composition-set.blade.php
+++ resources/views/components/product-page/composition-set.blade.php
@@ -1,28 +1,28 @@
-@if (!empty($data['compositionSet']))
-    <div class="product-desc__complectation">
+<div class="product-desc__complectation">
+    @if (!empty($data['compositionSet']))
         <h5 class="product-desc__complectation-title">Комплектация</h5>
         <div class="product-desc__complectation-wrapper">
             @foreach ($data['compositionSet'] as $element)
                 @if ($element['qty'] > 1)
                     <p>{{ $element['type'] }}:
-                       <a href="{{ $element['link'] }}">
-                           {{ $element['model'] }}
-                           @if ($element['article'] !== '' && $element['article'] != $element['model'])
-                               ({{ $element['article'] }})
-                           @endif
-                       </a> - {{ $element['qty'] }} шт.
+                        <a href="{{ $element['link'] }}">
+                            {{ $element['model'] }}
+                            @if ($element['article'] !== '' && $element['article'] != $element['model'])
+                                ({{ $element['article'] }})
+                            @endif
+                        </a> - {{ $element['qty'] }} шт.
                     </p>
                 @else
                     <p>{{ $element['type'] }}:
-                       <a href="{{ $element['link'] }}">
-                           {{ $element['model'] }}
-                           @if ($element['article'] !== '' && $element['article'] != $element['model'])
-                               ({{ $element['article'] }})
-                           @endif
-                       </a>
+                        <a href="{{ $element['link'] }}">
+                            {{ $element['model'] }}
+                            @if ($element['article'] !== '' && $element['article'] != $element['model'])
+                                ({{ $element['article'] }})
+                            @endif
+                        </a>
                     </p>
                 @endif
             @endforeach
         </div>
-    </div>
-@endif
+    @endif
+</div>
