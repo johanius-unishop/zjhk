03207308912e12b0cb918c11688b359a89f3aef5
johanius <info@kevtek.ru>
2025-08-11 15:03:39 +0300

    review section remake

--- app/Policies/AdminPolicy.php
+++ app/Policies/AdminPolicy.php
@@ -12,6 +12,7 @@ class AdminPolicy
      */
     public function isAdmin(User $user)
     {
+        dd($user->user_group_id );
         return $user->user_group_id === 4 ||
                $user->group->name === 'Администратор';
     }
