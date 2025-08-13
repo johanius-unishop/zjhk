6adb51e82c2d8dbb7334aa349d89b19bac1bbbf2
johanius <info@kevtek.ru>
2025-08-13 16:52:00 +0300

    recode Livewire Currency table

--- app/Livewire/CurrencyTable.php
+++ app/Livewire/CurrencyTable.php
@@ -61,49 +61,48 @@ final class CurrencyTable extends PowerGridComponent
     {
         return [
             Column::make('Id', 'id'),
-            Column::make('Name', 'name')
+            Column::make('Валюта', 'name')
                 ->sortable()
                 ->searchable()
                 ->editOnClick(hasPermission: true),
-            Column::make('Charcode', 'charcode')
+            Column::make('Код', 'charcode')
                 ->sortable()
                 ->searchable(),
 
-            Column::make('Cb rate', 'cb_rate')
-                ,
+            Column::make('Курс ЦБ', 'cb_rate'),
 
-            Column::make('Internal rate', 'internal_rate')
-                ,
+            Column::make('Внутренний курс', 'internal_rate'),
 
-            Column::make('Auto calc cbrf', 'auto_calc_cbrf')
+            Column::make('Автоматический расчет', 'auto_calc_cbrf')
+                ->sortable()
+                ->searchable()
+                ->toggleable(),
+
+            Column::make('Множитель', 'auto_multiplier')
                 ->sortable()
                 ->searchable(),
 
-            Column::make('Auto multiplier', 'auto_multiplier')
-                ->sortable()
-                ->searchable(),
 
-            Column::action('Action')
+            Column::action('Действия')
         ];
     }
 
     public function filters(): array
     {
-        return [
-        ];
+        return [];
     }
 
     #[\Livewire\Attributes\On('edit')]
     public function edit($rowId): void
     {
-        $this->js('alert('.$rowId.')');
+        $this->js('alert(' . $rowId . ')');
     }
 
     public function actions(Currency $row): array
     {
         return [
             Button::add('edit')
-                ->slot('Edit: '.$row->id)
+                ->slot('Edit: ' . $row->id)
                 ->id()
                 ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
                 ->dispatch('edit', ['rowId' => $row->id])
@@ -121,4 +120,77 @@ final class CurrencyTable extends PowerGridComponent
         ];
     }
     */
+    protected function rules()
+    {
+        return [
+            'name.*' => [
+                'required',
+            ],
+
+            'charcode.*' => [
+                'required',
+            ],
+
+            'auto_multiplier.*' => [
+                'numeric',
+                'min:0.5',
+                'max:1.5',
+            ],
+
+            'internal_rate.*' => [
+                'numeric',
+                'min:0.01',
+            ],
+
+        ];
+    }
+    protected function validationAttributes()
+    {
+        return [
+            'name.*'       => 'Название валюты',
+            'charcode.*' => 'Символьный код',
+            'auto_calc_cbrf.*' => 'Авторасчет внутреннего курса',
+            'auto_multiplier.*' => 'Множитель',
+            'internal_rate.*' => 'Внутренний курс',
+        ];
+    }
+
+    protected function messages()
+    {
+        return [
+            'name.*.required'     => 'Название обязательно должно быть заполнено',
+            'name.*.unique'     => 'Название должно быть уникальным',
+            'charcode.*.unique' => 'Валюта с таким символьным кодом уже есть в списке.',
+            'auto_multiplier.*.numeric' => 'Множитель должен быть числом, разделитель дробной части "."',
+            'auto_multiplier.*.max' => 'Множитель должен быть меньше или равен 1.5',
+            'auto_multiplier.*.min' => 'Множитель должен быть больше или равен 0.5',
+            'internal_rate.*.numeric' => 'Внутренний курс должен быть числом, разделитель дробной части "."',
+            'internal_rate.*.min' => 'Внутренний курс должен быть больше или равен 0.01',
+        ];
+    }
+
+    public function onUpdatedEditable(int|string $id, string $field, string $value): void
+    {
+        if (in_array($field, ['internal_rate', 'auto_multiplier'])) {
+            $value = round(str_replace(',', '.', trim($value)), 2);
+        }
+
+        $this->withValidator(function (\Illuminate\Validation\Validator $validator) use ($id, $field) {
+            if ($validator->errors()->isNotEmpty()) {
+                $this->dispatch('toggle-' . $field . '-' . $id);
+            }
+        })->validate();
+
+        $updated = Currency::query()->find($id)->update([
+            $field => $value,
+        ]);
+    }
+
+    public function onUpdatedToggleable(string|int $id, string $field, string $value): void
+    {
+        Currency::query()->find($id)->update([
+            $field => e($value),
+        ]);
+        $this->skipRender();
+    }
 }
