a556bf928b845b5fcd8870a87a8f56c34f76aa6b
johanius <info@kevtek.ru>
2025-08-13 21:03:45 +0300

    recode Livewire Currency table

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,12 +6,12 @@ import 'bootstrap'
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 
-import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+/*import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import Clipboard from '@ryangjchandler/alpine-clipboard'
 
 Alpine.plugin(Clipboard)
 
-Livewire.start()
+Livewire.start()*/
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
 
 /*
