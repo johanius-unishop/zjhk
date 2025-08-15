2cdb040ef2ebe08763c2bc4dbd4647c8fa51211a
johanius <info@kevtek.ru>
2025-08-15 21:11:41 +0300

    Catalog menu edit level 2

--- resources/views/components/header/header-bottom.blade.php
+++ resources/views/components/header/header-bottom.blade.php
@@ -228,12 +228,12 @@
                                                 <ul class="catalogue__category-grid">
                                                     @foreach ($menu_level_2_item->children as $menu_level_3_item)
                                                         <li>
-                                                            <a href="#">{{ $menu_level_3_item->name }}</a><span>24</span>
+                                                            <a href="#">{{ $menu_level_3_item->name }}</a>
                                                             @if ($menu_level_3_item->children->count() > 0)
                                                                 <ul>
                                                                     @foreach ($menu_level_3_item->children as $menu_level_4_item)
                                                                         <li>
-                                                                            <a href="#">{{ $menu_level_4_item->name }}</a><span>15</span>
+                                                                            <a href="#">{{ $menu_level_4_item->name }}</a>
                                                                         </li>
                                                                     @endforeach
                                                                 </ul>
