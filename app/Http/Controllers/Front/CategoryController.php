aeb215d5359958997709dd1ce5ae39b133e8372f
johanius <info@kevtek.ru>
2025-08-17 20:54:18 +0300

    Category show blade

--- app/Http/Controllers/Front/CategoryController.php
+++ app/Http/Controllers/Front/CategoryController.php
@@ -29,7 +29,7 @@ class CategoryController extends Controller
     {
         $category = Category::where('slug', $slug)->firstOrFail();
         $childrens = $category->childrens;
-        $parents = Category::ancestorsAndSelf($category)->toArray();
+        $parents = Category::ancestors($category)->toArray();
 
         $products = Product::where('category_id', $category->id)->with('media')->paginate(12)->withQueryString();
         // $products = $category->products->paginate(12);
