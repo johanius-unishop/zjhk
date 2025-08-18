e6f820478f1fe63f34f3f0668eead4698b952ff1
johanius <info@kevtek.ru>
2025-08-18 16:01:06 +0300

    rules for product price

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,13 @@ import * as Popper from '@popperjs/core'
 window.Popper = Popper
 import 'bootstrap'
 import 'bootstrap/dist/js/bootstrap.bundle';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import Clipboard from '@ryangjchandler/alpine-clipboard'
+
+Alpine.plugin(Clipboard)
+
+Livewire.start()
+
 import '../../vendor/power-components/livewire-powergrid/dist/powergrid'
 
 import '../js/front'
-
-
