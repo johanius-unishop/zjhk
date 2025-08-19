20116953e972f333c463f38e9a84865d78c765f1
johanius <info@kevtek.ru>
2025-08-19 21:24:04 +0300

    pagination

--- resources/views/front/category/show.blade.php
+++ resources/views/front/category/show.blade.php
@@ -180,15 +180,16 @@
                                                     <div class="swiper-pagination-product"></div>
                                                 </div>
                                                 <button class="product-page__label-btn">
-                                                    <img class="product-page__label" src="{{ asset('images/icons/label-gray.svg') }}"
-                                                        alt="избранное">
+                                                    <img class="product-page__label"
+                                                        src="{{ asset('images/icons/label-gray.svg') }}" alt="избранное">
                                                 </button>
                                             </div>
                                             <div data-layout class="product-page__title-container card-layout">
                                                 <h5>{{ $product_item->name }}</h5>
                                                 <p>{{ $product_item->article }}</p>
                                                 <div>
-                                                    <img src="{{ asset('images/icons/star.svg') }}" alt="рейтинг"><span>{{ $product_item->getAverageReviewRatingString() }}</span>
+                                                    <img src="{{ asset('images/icons/star.svg') }}"
+                                                        alt="рейтинг"><span>{{ $product_item->getAverageReviewRatingString() }}</span>
                                                     <a><span>{{ $product_item->getCountReviewsString() }}</span></a>
                                                 </div>
                                             </div>
@@ -205,51 +206,7 @@
                             <div class="product-page__show">
                                 <button>Показать еще</button>
                             </div>
-
-
-                            <div class="product-page__pagination">
-                                <div class="product-page__pagination-content">
-                                    {{ $data['products']->links() }}
-                                    <button>
-                                        <img src="./img/icons/tick-grey.svg" alt="влево">
-                                        <img src="./img/icons/tick-black.svg"
-                                            style="display: none; transform:rotateZ(180deg)" alt="влево">
-                                    </button>
-                                    <ul class="product-page__page-list">
-                                        <li data-pagination><button class="_active">1</button></li>
-                                        <li data-pagination><button>2</button></li>
-                                        <li data-pagination><button>3</button></li>
-                                        <li data-pagination><button>4</button></li>
-                                        <li data-pagination><button>5</button></li>
-                                        <li data-pagination><button>...</button></li>
-                                        <li data-pagination><button>21</button></li>
-                                    </ul>
-                                    <button>
-                                        <img src="{{ asset('images/icons/tick-grey.svg') }}"
-                                            style="display: none; transform:rotateZ(180deg)" alt="вправо">
-                                        <img src="{{ asset('images/icons/tick-black.svg') }}" alt="вправо">
-                                    </button>
-                                </div>
-                                <div class="product-page__page-size" data-page-size>
-                                    <div>
-                                        <p class="product-page__page-select">
-                                            <span>Показывать по </span>
-                                            <button class="product-page__change-btn">
-                                                <span id="page-count">6</span>
-                                                <img src="{{ asset('images/icons/tick-black.svg') }}" alt="список страниц">
-                                            </button>
-                                        </p>
-                                        <ul class="product-page__page-count" data-select-page>
-                                            <li><button data-page="6">6</button></li>
-                                            <li><button data-page="12">12</button></li>
-                                            <li><button data-page="24">24</button></li>
-                                            <li><button data-page="36">36</button></li>
-                                            <li><button data-page="48">48</button></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-
+                            {{ $data['products']->links() }}
                         </div>
                     </div>
                 </div>
