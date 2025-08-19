923c96c5d7b845e97a756aa04ede3c7b6697838b
johanius <info@kevtek.ru>
2025-08-19 23:15:50 +0300

    pagination

--- app/Http/Controllers/Front/CategoryController.php
+++ app/Http/Controllers/Front/CategoryController.php
@@ -49,7 +49,7 @@ class CategoryController extends Controller
         $sortOrder = $request->input('sort_by_price', '');
 
         // Основной запрос по товарам
-        $query = Product::where('category_id', $category->id)->where('published', 1)->orderBy('stock');
+        $query = Product::where('category_id', $category->id)->where('published', 1)->orderBy('stock', 'DESC');
 
         $childrens = Category::defaultOrder()
             ->where('parent_id', $category->id)
