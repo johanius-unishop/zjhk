21419f8a078dae23419aab3adafd8a608e8759b0
johanius <info@kevtek.ru>
2025-08-21 10:56:22 +0300

    pagination

--- app/Livewire/Front/ProductSelect.php
+++ app/Livewire/Front/ProductSelect.php
@@ -7,7 +7,7 @@ use Livewire\Component;
 class ProductSelect extends Component
 {
     public string $layoutType = 'list'; // Начальное состояние: список
-    public array $elements = [];
+    public $data;
 
     public function toggleLayout()
     {
