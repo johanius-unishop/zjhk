4dbba788dd7c7709787fbc477728c57a985981be
johanius <info@kevtek.ru>
2025-08-25 21:14:36 +0300

    pagination

--- resources/views/livewire/front/product-select/change-view.blade.php
+++ resources/views/livewire/front/product-select/change-view.blade.php
@@ -4,13 +4,13 @@
     </div>
     <div class="product-page__change-layout">
         <!-- Кнопка карточки -->
-        <button wire:click="toggleLayout" id="card-layout" class="toggle__layout">
+        <button wire:click="switchLayout" id="card-layout" class="toggle__layout">
             <img src="{{ asset('images/icons/block-gray.svg') }}" alt="Карточки"
                 class="change__img {{ $layoutType === 'card' ? '_active' : '' }}">
         </button>
 
         <!-- Кнопка списка -->
-        <button wire:click="toggleLayout" id="list-layout" class="toggle__layout">
+        <button wire:click="switchLayout" id="list-layout" class="toggle__layout">
             <img src="{{ asset('images/icons/list.svg') }}" alt="Список"
                 class="change__img {{ $layoutType === 'list' ? '_active' : '' }}">
         </button>
