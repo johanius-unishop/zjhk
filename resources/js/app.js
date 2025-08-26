3b98eb86813783633073a1ae7c2605f984b47194
johanius <info@kevtek.ru>
2025-08-26 11:52:05 +0300

    pagination

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,18 +20,6 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
 
-const swiperProduct = new Swiper('.product-page-slider', {
-	modules: [EffectFade, Pagination],
-	loop: false,
-	grabCursor: false,
-	pagination: {
-		el: '.swiper-pagination-product',
-		clickable: true,
-		renderBullet: function (index, className) {
-			return '<span class="' + className + '">' + '</span>'
-		}
-	}
-})
 
 
 
