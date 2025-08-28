e5072e3b1ebb8cf451605b5a28f436d0a9ce8e7c
johanius <info@kevtek.ru>
2025-08-28 23:40:58 +0300

    pagination

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -19,8 +19,26 @@
                 @if ($page == $paginator->currentPage())
                     <li data-pagination><button class="_active">{{ $page }}</button></li>
                 @else
-                    <li data-pagination><button
-                            onclick="window.location.href='{{ $url }}'">{{ $page }}</button></li>
+                    <li data-pagination>
+                        @php
+                            // Разбираем текущий URL и получаем массив параметров
+                            $parsedUrl = parse_url($url);
+                            $queryParams = [];
+                            parse_str($parsedUrl['query'], $queryParams);
+
+                            // Удаляем параметр reset_page из массива параметров
+                            unset($queryParams['reset_page']);
+
+                            // Формируем новый URL без параметра reset_page
+                            $newUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'] . $parsedUrl['path'];
+                            if (!empty($queryParams)) {
+                                $newUrl .= '?' . http_build_query($queryParams);
+                            }
+                        @endphp
+
+                        <button
+                            onclick="window.location.href='{{ $newUrl }}'">{{ $page }}</button>
+                    </li>
                 @endif
             @endforeach
         @endif
