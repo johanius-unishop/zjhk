ecc1191362faeee78783e707e40fb838c28ebfb1
johanius <info@kevtek.ru>
2025-08-29 20:38:37 +0300

    pagination

--- resources/views/front/category/show.blade.php
+++ resources/views/front/category/show.blade.php
@@ -48,57 +48,7 @@
                         @livewire('front.product-select.filter', ['filter' => $data['filter']])
                         <div class="product-page__layout">
                             @livewire('front.product-select.change-view', ['layoutType' => session()->get('layoutType', 'card')])
-                            <div id="layout" data-layout class="product-page__grid card-layout hide-subsequent-rows">
-                                @foreach ($data['products'] as $product_item)
-                                    <div class="product-page__item">
-                                        <div data-layout class="product-page__item-wrapper card-layout">
-                                            <div>
-                                                <div class="swiper product-page-slider">
-                                                    <div class="swiper-wrapper">
-                                                        @foreach ($product_item->getMedia('images') as $product_image)
-                                                            <div class="swiper-slide"><img
-                                                                    src="{{ $product_image->getUrl('thumb') }}"
-                                                                    alt="{{ $product_item->getAltAttribute() }}">
-                                                            </div>
-                                                        @endforeach
-                                                    </div>
-                                                    <div class="swiper-pagination-product"></div>
-                                                </div>
-                                                <button class="product-page__label-btn">
-                                                    <img class="product-page__label"
-                                                        src="{{ asset('images/icons/label-gray.svg') }}" alt="избранное">
-                                                </button>
-                                            </div>
-                                            <div data-layout class="product-page__title-container card-layout">
-                                                <h5>{{ $product_item['name'] }}</h5>
-                                                <p>{{ $product_item['article'] }}</p>
-                                                <div>
-                                                    @if ($product_item->reviews->count() != 0)
-                                                        <img src="{{ asset('images/icons/star.svg') }}"
-                                                            alt="рейтинг"><span>{{ $product_item->getAverageReviewRatingString() }}</span>
-                                                        <a><span>{{ $product_item->getCountReviewsString() }}</span></a>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                            <div data-layout class="product-page__info-container card-layout">
-                                                <p>{{ $product_item->getUserPrice() }}</p>
-
-                                                @if ($product_item->stock > 0)
-                                                    <p style="color: #027a48;">{{ $product_item->getUserStock() }}</p>
-                                                @elseif ($product_item->stock === 0)
-                                                    <p style="color: #6905ec;">{{ $product_item->getUserStock() }}</p>
-                                                @else
-                                                    <p style="color: #6905ec;">{{ $product_item->getUserStock() }}</p>
-                                                @endif
-
-                                                <button class="product-page__add-cart">В корзину</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endforeach
-
-                            </div>
-
+                            'html' => view('partials.products_list', ['data['products'] => $data['products']])->render()
 
 
                             <div class="product-page__show">
