cc80b9bab03230f941b0d06e783b158fc2bdd7ba
johanius <info@kevtek.ru>
2025-08-30 11:42:03 +0300

    pagination

--- app/Livewire/Front/ProductsSelect.php
+++ app/Livewire/Front/ProductsSelect.php
@@ -7,11 +7,30 @@ class ProductsSelect extends Component
 {
     public $filter;
 
+    public string $layoutType;
+
     public array $elements = [];
+
+    public function switchLayout()
+    {
+
+        if ($this->layoutType === 'card') {
+            $this->layoutType = 'list';
+        } else {
+            $this->layoutType = 'card';
+        }
+
+        // Записываем новый тип макета в сессию
+        session(['layoutType' => $this->layoutType]);
+
+        // Оповещаем слушателей об изменении макета
+        $this->dispatch('updateLayout', layoutType: $this->layoutType);
+    }
 
     public function mount($elements, $filter)
     {
         $this->filter = $filter;
+        $this->layoutType = session('layoutType', 'card'); // По умолчанию "card"
         $this->elements = $elements;
     }
 
