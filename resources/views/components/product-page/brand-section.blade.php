2c953915bf6332d6c158aba235cb9f29cb3c46e1
johanius <info@kevtek.ru>
2025-09-03 23:44:27 +0300

    webp format in product page

--- resources/views/components/product-page/brand-section.blade.php
+++ resources/views/components/product-page/brand-section.blade.php
@@ -1,28 +1,35 @@
 <div class="product-desc__brand">
-    @if($data['product']->vendor && trim($data['product']->vendor->short_name))
-        <h3>{{ $data['product']->vendor->short_name }} <img src="{{ asset('images/icons/attention-gray.svg') }}" alt="логотип"></h3>
+    @if ($data['product']->vendor && trim($data['product']->vendor->short_name))
+        <h3>{{ $data['product']->vendor->short_name }} <img src="{{ asset('images/icons/attention-gray.svg') }}"
+                alt="логотип"></h3>
         <a href="#">Все товары бренда</a>
-        @if ($data['acceptsWebP'] && $data['product']->vendor->getMedia('vendorLogo') && $data['product']->vendor->getMedia('vendorLogo')->hasGeneratedConversion('webp-thumb'))
-                        <img src="{{ $data['product']->vendor->getMedia('vendorLogo')->getUrl('webp-thumb') }}"
-                            alt="Миниатюра логотипа компании {{ $data['product']->vendor->short_name }}" loading="lazy">
-                    @elseif (!$data['acceptsWebP'] && $data['product']->vendor->getMedia('vendorLogo') && $data['product']->vendor->getMedia('vendorLogo')->hasGeneratedConversion('thumb'))
-                        <img src="{{ $data['product']->vendor->getMedia('vendorLogo')->getUrl('thumb') }}"
-                            alt="Миниатюра логотипа компании {{ $data['product']->vendor->short_name }}" loading="lazy">
-                    @else
-                        <img src="{{ $data['product']->vendor->getMedia('vendorLogo') ? $data['product']->vendor->getMedia('vendorLogo')->getUrl() : asset('/images/default_image.jpg') }}"
-                            alt=" Миниатюра логотипа компании {{ $data['product']->vendor->short_name }}" loading="lazy">
-                    @endif
+        @if (
+            $data['acceptsWebP'] &&
+                $data['product']->vendor->getFirstMedia('vendorLogo') &&
+                $data['product']->vendor->getFirstMedia('vendorLogo')->hasGeneratedConversion('webp-thumb'))
+            <img src="{{ $data['product']->vendor->getFirstMedia('vendorLogo')->getUrl('webp-thumb') }}"
+                alt="Миниатюра логотипа компании {{ $data['product']->vendor->short_name }}" loading="lazy">
+        @elseif (
+            !$data['acceptsWebP'] &&
+                $data['product']->vendor->getFirstMedia('vendorLogo') &&
+                $data['product']->vendor->getFirstMedia('vendorLogo')->hasGeneratedConversion('thumb'))
+            <img src="{{ $data['product']->vendor->getFirstMedia('vendorLogo')->getUrl('thumb') }}"
+                alt="Миниатюра логотипа компании {{ $data['product']->vendor->short_name }}" loading="lazy">
+        @else
+            <img src="{{ $data['product']->vendor->getFirstMedia('vendorLogo') ? $data['product']->vendor->getFirstMedia('vendorLogo')->getUrl() : asset('/images/default_image.jpg') }}"
+                alt=" Миниатюра логотипа компании {{ $data['product']->vendor->short_name }}" loading="lazy">
+        @endif
 
     @endif
 </div>
 
 <div class="product-desc__country">
-    @if($data['product']->vendor && $data['product']->vendor->country && trim($data['product']->vendor->country->name))
+    @if ($data['product']->vendor && $data['product']->vendor->country && trim($data['product']->vendor->country->name))
         <p><img src="{{ asset('images/icons/china_flag.svg') }}" alt="страна"> <span>Страна производства:
                 {{ $data['product']->vendor->country->name }}</span></p>
     @endif
 
-    @if($data['product']->vendor && isset($data['product']->vendor->warranty) && trim($data['product']->vendor->warranty))
+    @if ($data['product']->vendor && isset($data['product']->vendor->warranty) && trim($data['product']->vendor->warranty))
         <p><img src="{{ asset('images/icons/protect.svg') }}" alt="гарантия"> <span>Гарантия производителя:
                 {{ $data['product']->vendor->warranty }}</span></p>
     @endif
