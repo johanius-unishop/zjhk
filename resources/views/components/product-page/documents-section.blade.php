f79483e7ad0f5a58859be2bd876ef479c4ca0883
johanius <info@kevtek.ru>
2025-09-04 10:28:11 +0300

    fix documetation section

--- resources/views/components/product-page/documents-section.blade.php
+++ resources/views/components/product-page/documents-section.blade.php
@@ -1,11 +1,11 @@
 <div class="product-desc__docs">
     <h5 class="product-desc__docs-title">Документация</h5>
     <div class="product-desc__docs-wrapper">
-        <p><img src="{{ asset('images/icons/note.svg') }}" alt="характеристики"><span><a>Технические характеристики НA-016</a></span></p>
+        <a><p><img src="{{ asset('images/icons/note.svg') }}" alt="характеристики"><span>Технические характеристики НA-016</span></p></a>
         <p><img src="{{ asset('images/icons/note.svg') }}" alt="характеристики"><span>Вырезка из каталога по серии НА-016</span></p>
         <p><img src="{{ asset('images/icons/ruler.svg') }}" alt="чертеж"><span>Габаритный чертёж</span></p>
         <p><img src="{{ asset('images/icons/3d.svg') }}" alt="3д-модель"><span>3D-модель</span></p>
         <a href="#" download="#"><img src="{{ asset('images/icons/download.svg') }}"
-                alt="скачать"><span>Скачать всё</span></a>
+                alt="скачать"><span>Скачать всё</span></a>
     </div>
 </div>
