6b1bd8e3989f3daa2ff0f4193f6c2bc2cb388606
johanius <info@kevtek.ru>
2025-09-11 21:23:48 +0300

    fix sliders on product page

--- resources/views/components/product-page/documents-section.blade.php
+++ resources/views/components/product-page/documents-section.blade.php
@@ -1,11 +1,23 @@
 <div class="product-desc__docs">
     <h5 class="product-desc__docs-title">Документация</h5>
     <div class="product-desc__docs-wrapper">
-        <a><p><img src="{{ asset('images/icons/note.svg') }}" alt="характеристики"><span>Технические характеристики НA-016</span></p></a>
-        <p><img src="{{ asset('images/icons/note.svg') }}" alt="характеристики"><span>Вырезка из каталога по серии НА-016</span></p>
-        <p><img src="{{ asset('images/icons/ruler.svg') }}" alt="чертеж"><span>Габаритный чертёж</span></p>
-        <p><img src="{{ asset('images/icons/3d.svg') }}" alt="3д-модель"><span>3D-модель</span></p>
-        <a href="#" download="#"><img src="{{ asset('images/icons/download.svg') }}"
-                alt="скачать"><span>Скачать всё</span></a>
+        <p><img src="{{ asset('images/icons/note.svg') }}" alt="характеристики"><a href="#">
+                <span>Технические характеристики
+                    НA-016</span>
+            </a>
+        </p>
+        <p><img src="{{ asset('images/icons/note.svg') }}" alt="характеристики"><a href="#"> <span>Вырезка из каталога
+                    по серии НА-016</span>
+            </a>
+        </p>
+        <p><img src="{{ asset('images/icons/ruler.svg') }}" alt="чертеж"> <a href="#">
+                <span>Габаритный чертёж</span>
+            </a>
+        </p>
+        <p><img src="{{ asset('images/icons/3d.svg') }}" alt="3д-модель"><a href="#">
+                <span>3D-модель</span>
+            </a>
+        </p>
+        <a href="#" download="#"><img src="{{ asset('images/icons/download.svg') }}" alt="скачать"><span>Скачать всё</span></a>
     </div>
 </div>
