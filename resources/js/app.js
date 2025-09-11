d089c35ad8be7e4706aff2e8ab2bc3aed3e3dadd
johanius <info@kevtek.ru>
2025-09-11 16:53:39 +0300

    fix sliders on product page

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,8 @@ window.Swiper = Swiper
 window.EffectFade = EffectFade
 window.Pagination = Pagination
 window.Navigation = Navigation
+window.Thumbs = Thumbs
+window.Autoplay = Autoplay
 
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import Clipboard from '@ryangjchandler/alpine-clipboard'
