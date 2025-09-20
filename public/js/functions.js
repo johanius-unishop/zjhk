7461dfc17f8321e9c6664656212a61d1c8913cba
johanius <info@kevtek.ru>
2025-09-20 09:53:43 +0300

    add js header

---
+++ public/js/functions.js
@@ -0,0 +1,41 @@
+export const getLog = (text, value) => {
+    console.log(`${text} ${value}`);
+};
+
+export const addClass = (element, className) => {
+    element?.classList.add(className);
+};
+
+export const removeClass = (element, className) => {
+    element?.classList.remove(className);
+};
+
+export const toggleClass = (element, className) => {
+    element?.classList.toggle(className);
+};
+
+export const isTargetElement = (element, eventTarget) => {
+    return element?.contains(eventTarget);
+};
+
+export const decrementCounter = (initialValue, renderValueEl) => {
+    if (initialValue > 1) {
+        initialValue--;
+    }
+    renderValueEl.textContent = initialValue;
+    return initialValue;
+};
+
+export const addPadding = (htmlElem) => {
+    const htmlElemPadding = parseInt(
+        window.getComputedStyle(htmlElem).paddingRight,
+        10
+    );
+    const scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+    const totalPadding = scrollBarWidth + htmlElemPadding + "px";
+    htmlElem.style.paddingRight = totalPadding;
+};
+
+export const removePadding = (htmlElem, initialPadding) => {
+    htmlElem.style.paddingRight = initialPadding;
+};
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,12 @@
 <header id="header" class="header">
-   @include('components.header.header-top')
-   @include('components.header.header-bottom')
+    @include('components.header.header-top')
+    @include('components.header.header-bottom')
 </header>
 <div id="header-mobile" class="header-mobile">
-   @include('components.header.header-mobile-top')
-   @include('components.header.header-mobile-bottom')
+    @include('components.header.header-mobile-top')
+    @include('components.header.header-mobile-bottom')
 </div>
+
+@push('scripts')
+    <script src="{{ asset('js/header.js') }}"></script>
+@endpush
