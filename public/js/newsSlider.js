07b4c8ac2c0de3fbdbba1e1c98255d4f1e1c367b
johanius <info@kevtek.ru>
2025-09-20 10:53:57 +0300

    add news slider

--- public/js/newsSlider.js
+++ public/js/newsSlider.js
@@ -1,22 +1,24 @@
-const sliderNews = new Swiper('.news-slider', {
-	modules: [EffectFade, Navigation],
-	navigation: {
-		nextEl: '.swiper-button-next.news',
-		prevEl: '.swiper-button-prev.news'
-	},
-	loop: true,
-	breakpoints: {
-		360: {
-			slidesPerView: 2,
-			spaceBetween: 8
-		},
-		680: {
-			slidesPerView: 3,
-			spaceBetween: 20
-		},
-		992: {
-			slidesPerView: 4,
-			spaceBetween: 20
-		}
-	}
-})
+document.addEventListener("DOMContentLoaded", () => {
+    const sliderNews = new Swiper(".news-slider", {
+        modules: [EffectFade, Navigation],
+        navigation: {
+            nextEl: ".swiper-button-next.news",
+            prevEl: ".swiper-button-prev.news",
+        },
+        loop: true,
+        breakpoints: {
+            360: {
+                slidesPerView: 2,
+                spaceBetween: 8,
+            },
+            680: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+        },
+    });
+});
