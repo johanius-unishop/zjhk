e64cbfc0bbc4f70269cae6b47486dc53c98d17ec
johanius <info@kevtek.ru>
2025-09-21 17:00:16 +0300

    add livewire searchResult

--- app/Livewire/Front/SearchResult.php
+++ app/Livewire/Front/SearchResult.php
@@ -2,24 +2,24 @@
 
 namespace App\Livewire\Front;
 
-
 use Livewire\Component;
-use Illuminate\Support\Facades\DB;
-use App\Models\Product as ModelsProduct;
+use App\Models\Product;
 
 class SearchResult extends Component
 {
-    public $search = '';
-    public $searchResults;
-    public $visible = false;
+    public $search = '';   // Поле ввода поиска
+    public $searchResults; // Результаты поиска
+    public $visible = false; // Видимость результатов
 
     public function render()
     {
+        // Проверяем длину очищенного поискового запроса
         if (strlen(trim($this->search)) > 2) {
             $this->visible = true;
-            $this->searchResults = ModelsProduct::search($this->search)->get();
+            $this->searchResults = Product::search($this->search)->get();
         } else {
             $this->visible = false;
+            $this->searchResults = [];
         }
 
         return view('livewire.front.search-result');
