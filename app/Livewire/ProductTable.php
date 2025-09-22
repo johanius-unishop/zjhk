a1b598603ea695a98a9df7f34768dd5984436aa5
johanius <info@kevtek.ru>
2025-09-22 18:48:31 +0300

    add livewire searchResult

--- app/Livewire/ProductTable.php
+++ app/Livewire/ProductTable.php
@@ -135,7 +135,7 @@ final class ProductTable extends PowerGridComponent
             $buttons[] = Button::add('view')
                 ->slot('<i class="fas fa-edit"></i>')
                 ->class('btn btn-primary')
-                ->route('admin.product.edit', ['product' => $row->id]);
+                ->route('admin.product.edit', ['product' => $row->slug]);
         }
 
         return $buttons;
