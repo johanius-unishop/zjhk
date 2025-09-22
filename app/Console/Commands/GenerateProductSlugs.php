c9a86205dd90e6666559a4bec5f36e6716d2c6b0
johanius <info@kevtek.ru>
2025-09-22 16:04:10 +0300

    add livewire searchResult

---
+++ app/Console/Commands/GenerateProductSlugs.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Product;
+
+class GenerateProductSlugs extends Command
+{
+    protected $signature = 'products:generate-slugs';
+    protected $description = 'Generate slugs for products without slugs';
+
+    public function handle()
+    {
+        $productsWithoutSlug = Product::whereNull('slug')->get();
+
+        foreach ($productsWithoutSlug as $product) {
+            $product->save(); // При сохранении модели slug будет сгенерирован автоматически
+        }
+
+        $this->info('Slugs generated successfully!');
+    }
+}
