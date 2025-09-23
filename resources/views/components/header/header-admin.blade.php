f36b4411af1c845956b7851a005a262a1e827125
johanius <info@kevtek.ru>
2025-09-23 22:32:53 +0300

    add livewire searchResult

--- resources/views/components/header/header-admin.blade.php
+++ resources/views/components/header/header-admin.blade.php
@@ -1,11 +1,11 @@
 <div class="header__top-admin">
     <div class="header__container container">
         <div class="header__admin-wrapper">
-            <a href="#" class="header__admin-link">
+            <a href="{{ route('admin.dashboard') }}" class="header__admin-link">
                 <span>Администратор</span>
             </a>
-            <a href="#" class="header__admin-link">
-                <span>ivandeev@gmailc.com</span>
+            <a href="{{ route('admin.dashboard') }}" class="header__admin-link">
+                <span>{{ Auth::user()->email }}</span>
             </a>
         </div>
     </div>
