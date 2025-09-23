936baf804a6555203db8f1c1018125360822f9c3
johanius <info@kevtek.ru>
2025-09-23 21:42:19 +0300

    add livewire searchResult

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,12 +16,7 @@ window.Navigation = Navigation
 window.Thumbs = Thumbs
 window.Autoplay = Autoplay
 
-import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import Clipboard from '@ryangjchandler/alpine-clipboard'
 
-Alpine.plugin(Clipboard)
-
-Livewire.start()
 
 import '../../vendor/power-components/livewire-powergrid/dist/powergrid'
 
