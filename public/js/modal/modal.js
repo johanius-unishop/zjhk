5b6b9fa61e35e6eeb4bc65de56b2e30849a14354
johanius <info@kevtek.ru>
2025-09-24 10:57:22 +0300

    add accountPage

---
+++ public/js/modal/modal.js
@@ -0,0 +1,90 @@
+const modals = document.querySelectorAll('[data-modal]')
+const modalCallBtns = document.querySelectorAll('[data-modal-link]')
+const body = document.body
+
+const passwordInputs = document.querySelectorAll('.input-password');
+const toggleButtons = document.querySelectorAll('.password-toggle');
+const passwordIcons = document.querySelectorAll('.password-toggle img');
+
+passwordInputs.forEach((input, index) => {
+    const toggleButton = toggleButtons[index];
+    const icon = passwordIcons[index];
+
+    input.addEventListener('input', function() {
+        if (input.value.length > 0) {
+            toggleButton.classList.add('_active');
+        } else {
+            toggleButton.classList.remove('_active');
+        }
+    });
+
+    toggleButton.addEventListener('click', function() {
+        const isPassword = input.type === 'password';
+        input.type = isPassword ? 'text' : 'password';
+        icon.src = isPassword
+            ? './img/icons/password-eye.svg'
+            : './img/icons/password-eye-cross.svg';
+    });
+});
+
+const getScrollbarWidth = () => {
+	return window.innerWidth - document.documentElement.clientWidth
+}
+
+const setBodyStyle = () => {
+	const scrollbarWidth = getScrollbarWidth()
+	body.style.overflowY = 'hidden'
+	body.style.position = 'fixed'
+	body.style.top = `-${scrollPosition}px`
+	body.style.width = '100%'
+	body.style.paddingRight = `${scrollbarWidth}px`
+}
+
+const setBodyStyleDefault = () => {
+	body.style.overflowY = 'auto'
+	body.style.position = ''
+	body.style.top = ''
+	body.style.width = ''
+	body.style.paddingRight = '0'
+}
+
+let scrollPosition = 0
+
+const openModal = (modal) => {
+	if (!modal.classList.contains('_open')) {
+		scrollPosition = window.scrollY || document.documentElement.scrollTop
+		setBodyStyle()
+		modal.classList.add('_open')
+	}
+}
+
+const closeModal = (modal) => {
+	modal.classList.remove('_open')
+	setBodyStyleDefault()
+	window.scrollTo(0, scrollPosition)
+}
+
+if (modals.length > 0) {
+	modalCallBtns.forEach((btn) => {
+		btn.addEventListener('click', (e) => {
+			e.preventDefault()
+			const modalName = btn.dataset.modalLink
+			const targetModal = document.querySelector(`[data-modal="${modalName}"]`)
+			if (targetModal) {
+				openModal(targetModal)
+			} else {
+				console.error(`Модальное окно с data-modal="${modalName}" не найдено!`)
+			}
+		})
+	})
+
+	modals.forEach((modalWindow) => {
+		modalWindow.addEventListener('click', (e) => {
+			if (e.target.dataset.close) {
+				closeModal(modalWindow)
+			}
+		})
+	})
+}
+
+console.log('Modal is ready...')
--- resources/views/partials/login.blade.php
+++ resources/views/partials/login.blade.php
@@ -1,58 +1,67 @@
 <div data-modal="account" class="modal" id="loginModal">
-                <div data-close="true" class="modal__overlay">
-                    <div class="modal__body">
-                        <span data-close="true" class="modal__close">✖</span>
-                        <div class="modal__content">
-                            <h1 class="modal__title">Войти</h1>
-                            <form method="POST" action="{{ route('login') }}" class="modal__form">
-                                @csrf
-                            <div class="modal__input-container">
-                                <div>
-                                    <label for="input-email" class="visually-hidden"></label>
-                                    <input type="email" id="input-email" name="email" value="{{ old('email') }}" class="modal__input" placeholder="Введите email" required autocomplete="username">
-                                    <p class="modal__error" aria-live="polite">
-                                        <span hidden>{{ session('errors.email') ?? '' }}</span>
-                                    </p>
-                                </div>
-                                <div>
-                                    <label for="input-password" class="visually-hidden"></label>
-                                    <input type="password" id="input-password" name="password" placeholder="Введите пароль" autocomplete="current-password" value="{{ old('password') }}" >
-
-                                    <button type="button" id="toggle-password" class="password-toggle">
-                                        <img src=""{{ asset('images/icons/password-eye-cross.svg') }} alt="Показать пароль">
-                                    </button>
-                                    <p class="modal__error"><span hidden>{{ session('errors.password') ?? '' }}</span></p>
-                                </div>
-
-                            </div>
-                            <div>
-                                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Запомнить меня') }}
-                                    </label>
-                                </div>
-                            <div>
-                                <a href="/forgot-password" class="modal__forget-password">Забыли пароль?</a>
-                            </div>
-                            <button type="submit" class="modal__btn">Войти</button>
-                            </form>
-                            <p class="modal__policy">
-                            Нажимая кнопку «Войти», вы соглашаетесь c условиями <a href="#">политики конфиденциальности</a>
+    <div data-close="true" class="modal__overlay">
+        <div class="modal__body">
+            <span data-close="true" class="modal__close">✖</span>
+            <div class="modal__content">
+                <h1 class="modal__title">Войти</h1>
+                <form method="POST" action="{{ route('login') }}" class="modal__form">
+                    @csrf
+                    <div class="modal__input-container">
+                        <div>
+                            <label for="input-email" class="visually-hidden"></label>
+                            <input type="email" id="input-email" name="email" value="{{ old('email') }}"
+                                class="modal__input" placeholder="Введите email" required autocomplete="username">
+                            <p class="modal__error" aria-live="polite">
+                                <span hidden>{{ session('errors.email') ?? '' }}</span>
                             </p>
                         </div>
+                        <div>
+                            <label for="input-password" class="visually-hidden"></label>
+                            <input type="password" id="input-password" name="password" placeholder="Введите пароль"
+                                autocomplete="current-password" value="{{ old('password') }}">
+
+                            <button type="button" id="toggle-password" class="password-toggle">
+                                <img src=""{{ asset('images/icons/password-eye-cross.svg') }}
+                                    alt="Показать пароль">
+                            </button>
+                            <p class="modal__error"><span hidden>{{ session('errors.password') ?? '' }}</span></p>
+                        </div>
+
                     </div>
-                </div>
+                    <div>
+                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="remember">
+                            {{ __('Запомнить меня') }}
+                        </label>
+                    </div>
+                    <div>
+                        <a href="/forgot-password" class="modal__forget-password">Забыли пароль?</a>
+                    </div>
+                    <button type="submit" class="modal__btn">Войти</button>
+                </form>
+                <p class="modal__policy">
+                    Нажимая кнопку «Войти», вы соглашаетесь c условиями <a href="#">политики
+                        конфиденциальности</a>
+                </p>
             </div>
+        </div>
+    </div>
 </div>
 
+
 @section('scripts')
-@parent
-@if($errors->has('email') || $errors->has('password'))
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <script>
-    $(function() {
-        $('#loginModal').addClass("_open");
-    });
-    </script>
-@endif
+    @parent
+    <script src="{{ asset('js/modal/modal.js') }}"></script>
+
+
+
+
+    @if ($errors->has('email') || $errors->has('password'))
+        <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+        <script>
+            $(function() {
+                $('#loginModal').addClass("_open");
+            });
+        </script>
+    @endif
 @endsection
