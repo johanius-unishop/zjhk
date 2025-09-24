7ca8b1925cf157723106b490ca0e60dbee4fe263
johanius <info@kevtek.ru>
2025-09-24 11:25:33 +0300

    add accountPage

--- public/js/scrollToAncorLink.js
+++ public/js/scrollToAncorLink.js
@@ -1,32 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-        anchor.addEventListener("click", function (event) {
-            event.preventDefault(); // предотвращаем стандартное поведение браузера
+    document.querySelectorAll('a[href^="#"]:not(.no-scroll-to-handle)').forEach((anchor) => { // Исключаем элементы с классом no-scroll-to-handle
+        anchor.addEventListener("click", function(event) {
+            event.preventDefault(); // Предотвращаем стандартное поведение браузера
 
-            const targetId = this.getAttribute("href"); // получаем ID цели
-            const targetElement = document.querySelector(targetId); // находим целевой элемент
-            if (!targetElement) return; // проверяем существование элемента
+            const targetId = this.getAttribute("href"); // Получаем ID цели
+            const targetElement = document.querySelector(targetId); // Находим целевой элемент
+            if (!targetElement) return; // Проверяем наличие элемента
 
             const HEADER_FULL_HEIGHT = 110;
             const HEADER_SCROLL_HEIGHT = 64;
-            let scrollOffset = 0; // начальное значение смещения
+            let scrollOffset = 0; // Начальное значение смещения
 
             const bottomHeader = document.querySelector(".header__bottom");
-            // Для начала смотрим, активирована ли фиксация хидера (_scroll)
+            // Проверяем, активирован ли режим фиксации хедера (_scroll)
             if (bottomHeader && bottomHeader.classList.contains("_scroll")) {
                 scrollOffset = HEADER_SCROLL_HEIGHT;
-            }
-            else{
+            } else {
                 scrollOffset = HEADER_FULL_HEIGHT;
             }
 
-            // Итоговая позиция должна учитывать смещение
+            // Рассчитываем позицию с учетом смещения
             const targetPosition = targetElement.offsetTop - scrollOffset;
 
-            // Переводим страницу в нужное положение
+            // Перемещаемся на нужную позицию
             window.scrollTo({
                 top: targetPosition,
-                behavior: "smooth", // Гладкий скролл
+                behavior: "smooth", // Плавный скролл
             });
         });
     });
