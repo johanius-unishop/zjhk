4a4814d61a59b57c282a40602d3e8626a6eb18a2
johanius <info@kevtek.ru>
2025-09-28 20:41:08 +0300

    add accountPage

--- public/js/modal/modal.js
+++ public/js/modal/modal.js
@@ -149,6 +149,24 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    const signInLink = document.querySelector('.switch-to-signin');
+    const signUpLink = document.querySelector('.switch-to-signup');
+    const formsContainer = document.querySelector('.modal__body'); // Родительский контейнер обоих форм
+
+    // Переключение на форму регистрации
+    signUpLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        formsContainer.querySelector('.form-signin').style.display = 'none'; // Прячем форму входа
+        formsContainer.querySelector('.form-signup').style.display = 'block'; // Показываем форму регистрации
+    });
+
+    // Переключение обратно на форму входа
+    signInLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        formsContainer.querySelector('.form-signin').style.display = 'block'; // Показываем форму входа
+        formsContainer.querySelector('.form-signup').style.display = 'none'; // Прячем форму регистрации
+    });
+
 
 
     console.log("Modal is ready...");
--- routes/auth.php
+++ routes/auth.php
@@ -52,5 +52,4 @@ Route::middleware('auth')->group(function () {
     })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 });
 
-// Очистите кеш маршрутов после внесённых изменений
-// php artisan route:clear
+
