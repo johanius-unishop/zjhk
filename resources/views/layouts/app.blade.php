b90c72bd26cec1825829a0a1397f96d07a27135c
johanius <info@kevtek.ru>
2025-09-28 21:24:32 +0300

    add accountPage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,9 +43,42 @@
                 // Получить коллекцию модальных окон
                 const accountModal = document.querySelector('[data-modal="account"]');
 
+                const setBodyStyle = () => {
+                    const scrollbarWidth = getScrollbarWidth();
+                    body.style.overflowY = "hidden";
+                    body.style.position = "fixed";
+                    body.style.top = `-${scrollPosition}px`;
+                    body.style.width = "100%";
+                    body.style.paddingRight = `${scrollbarWidth}px`;
+                };
+
+                const setBodyStyleDefault = () => {
+                    body.style.overflowY = "auto";
+                    body.style.position = "";
+                    body.style.top = "";
+                    body.style.width = "";
+                    body.style.paddingRight = "0";
+                };
+
+                let scrollPosition = 0;
+
+                const openModal = (modal) => {
+                    if (!modal.classList.contains("_open")) {
+                        scrollPosition =
+                            window.scrollY || document.documentElement.scrollTop;
+                        setBodyStyle();
+                        modal.classList.add("_open");
+                    }
+                };
+
+                const closeModal = (modal) => {
+                    modal.classList.remove("_open");
+                    setBodyStyleDefault();
+                    window.scrollTo(0, scrollPosition);
+                };
+
                 console.log(accountModal);
                 openModal(accountModal);
-
             @endif
         });
     </script>
