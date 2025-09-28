31fcc0684d3daf19f7f91980c150e054de0b0416
johanius <info@kevtek.ru>
2025-09-28 20:46:10 +0300

    add accountPage

--- public/js/modal/modal.js
+++ public/js/modal/modal.js
@@ -122,6 +122,26 @@ document.addEventListener("DOMContentLoaded", () => {
         window.scrollTo(0, scrollPosition);
     };
 
+    const signInLink = document.querySelector('.switch-to-signin');
+    const signUpLink = document.querySelector('.switch-to-signup');
+    const formsContainer = document.querySelector('.modal__body'); // Родительский контейнер обоих форм
+    console.log(signUpLink)
+    // Переключение на форму регистрации
+    signUpLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        formsContainer.querySelector('.form-signin').style.display = 'none'; // Прячем форму входа
+        formsContainer.querySelector('.form-signup').style.display = 'block'; // Показываем форму регистрации
+    });
+
+    // Переключение обратно на форму входа
+    signInLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        formsContainer.querySelector('.form-signin').style.display = 'block'; // Показываем форму входа
+        formsContainer.querySelector('.form-signup').style.display = 'none'; // Прячем форму регистрации
+    });
+
+
+
     if (modals.length > 0) {
         modalCallBtns.forEach((btn) => {
             btn.addEventListener("click", (e) => {
@@ -148,24 +168,6 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     }
-
-    const signInLink = document.querySelector('.switch-to-signin');
-    const signUpLink = document.querySelector('.switch-to-signup');
-    const formsContainer = document.querySelector('.modal__body'); // Родительский контейнер обоих форм
-    console.log(signUpLink)
-    // Переключение на форму регистрации
-    signUpLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        formsContainer.querySelector('.form-signin').style.display = 'none'; // Прячем форму входа
-        formsContainer.querySelector('.form-signup').style.display = 'block'; // Показываем форму регистрации
-    });
-
-    // Переключение обратно на форму входа
-    signInLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        formsContainer.querySelector('.form-signin').style.display = 'block'; // Показываем форму входа
-        formsContainer.querySelector('.form-signup').style.display = 'none'; // Прячем форму регистрации
-    });
 
 
 
