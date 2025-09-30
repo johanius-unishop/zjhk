688f4f90047dbafa5ef61705859cf90a712c906e
johanius <info@kevtek.ru>
2025-09-30 23:22:25 +0300

    add accountPage

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -6,6 +6,7 @@
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
+<img src="{{ asset('images/icons/logo.svg') }}" alt="Logo Kevtek" width="150" height="auto">
 @endif
 </a>
 </td>
