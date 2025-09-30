217d9918bf298cc23445fa0c5cb7b1206adb92cf
johanius <info@kevtek.ru>
2025-09-30 20:17:15 +0300

    add accountPage

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -36,21 +36,15 @@
 {{-- Salutation --}}
 @if (! empty($salutation))
 {{ $salutation }}
-@else
-@lang('С уважением,')<br>
-@lang('коллектив ООО "Кевтек"')
 @endif
 
 {{-- Subcopy --}}
-@isset($actionText)
+
 <x-slot:subcopy>
-@lang(
-    "Если у вас возникли проблемы при нажатии кнопки \":actionText\", скопируйте и вставьте URL ниже\n".
-    'в свой веб-браузер:',
-    [
-        'actionText' => __('Сбросить пароль'),
-    ]
-) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+@if (! empty($markdown))
+{{ $markdown }}
+@endif
+<span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
 </x-slot:subcopy>
-@endisset
+
 </x-mail::message>
