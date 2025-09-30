9130e00e71d6a0968bf0e10d29c13d4b27d37216
johanius <info@kevtek.ru>
2025-09-30 19:35:26 +0300

    add accountPage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -57,7 +57,7 @@ class AppServiceProvider extends ServiceProvider
 
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
             return (new MailMessage)
-                ->subject('Verify Email Address')
+                ->subject('Verify Email Address111')
                 ->line('Click the button below to verify your email address.')
                 ->action('Verify Email Address', $url);
         });
