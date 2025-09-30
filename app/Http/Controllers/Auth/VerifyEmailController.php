fedb79c800e629e95c1c36549de67829d19c53f8
johanius <info@kevtek.ru>
2025-09-30 14:05:21 +0300

    add accountPage

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -16,7 +16,7 @@ class VerifyEmailController extends Controller
 {
     public function __invoke(Request $request)
     {
-        dd('наконец-то, мы тут!');
+        dd($request);
         Validator::make($request->all(), [
             'id' => 'required|exists:users,id',
             'hash' => 'required|string'
