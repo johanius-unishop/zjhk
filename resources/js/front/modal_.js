cd58188ff30597f38b44c88fc4e817e3b125ca40
johanius <info@kevtek.ru>
2025-10-02 22:59:46 +0300

    add accountPage

--- resources/js/front/modal_.js
+++ resources/js/front/modal_.js
@@ -1,242 +1,240 @@
 const modals = document.querySelectorAll("[data-modal]");
 
-    const modalCallBtns = document.querySelectorAll("[data-modal-link]");
+const modalCallBtns = document.querySelectorAll("[data-modal-link]");
 
-    const body = document.body;
+const body = document.body;
 
-    // Пароли для входа
-    const passwordLogin = document.getElementById("input-password-login");
-    const toggleButtonLogin = document.getElementById("toggle-password-login");
-    const visibleEyeLogin = toggleButtonLogin.querySelector(".visible-icon");
-    const hiddenEyeLogin = toggleButtonLogin.querySelector(".hidden-icon");
+// Пароли для входа
+const passwordLogin = document.getElementById("input-password-login");
+const toggleButtonLogin = document.getElementById("toggle-password-login");
+const visibleEyeLogin = toggleButtonLogin.querySelector(".visible-icon");
+const hiddenEyeLogin = toggleButtonLogin.querySelector(".hidden-icon");
 
-    checkPasswordState(passwordLogin, toggleButtonLogin);
+checkPasswordState(passwordLogin, toggleButtonLogin);
 
-    // Пароли для регистрации
-    const passwordReg = document.getElementById("input-password-reg");
-    const toggleButtonReg = document.getElementById("toggle-password-reg");
-    const visibleEyeReg = toggleButtonReg.querySelector(".visible-icon");
-    const hiddenEyeReg = toggleButtonReg.querySelector(".hidden-icon");
+// Пароли для регистрации
+const passwordReg = document.getElementById("input-password-reg");
+const toggleButtonReg = document.getElementById("toggle-password-reg");
+const visibleEyeReg = toggleButtonReg.querySelector(".visible-icon");
+const hiddenEyeReg = toggleButtonReg.querySelector(".hidden-icon");
 
-    checkPasswordState(passwordReg, toggleButtonReg);
+checkPasswordState(passwordReg, toggleButtonReg);
 
-    // Пароли для регистрации (подтверждение пароля)
-    const passwordRegConf = document.getElementById(
-        "input-password-reg-confirmation"
-    );
-    const toggleButtonRegConf = document.getElementById(
-        "toggle-password-reg-confirmation"
-    );
-    const visibleEyeRegConf =
-        toggleButtonRegConf.querySelector(".visible-icon");
-    const hiddenEyeRegConf = toggleButtonRegConf.querySelector(".hidden-icon");
+// Пароли для регистрации (подтверждение пароля)
+const passwordRegConf = document.getElementById(
+    "input-password-reg-confirmation"
+);
+const toggleButtonRegConf = document.getElementById(
+    "toggle-password-reg-confirmation"
+);
+const visibleEyeRegConf = toggleButtonRegConf.querySelector(".visible-icon");
+const hiddenEyeRegConf = toggleButtonRegConf.querySelector(".hidden-icon");
 
-    checkPasswordState(passwordRegConf, toggleButtonRegConf);
+checkPasswordState(passwordRegConf, toggleButtonRegConf);
 
-    // Пароли для Смены пароля
-    const passwordReset = document.getElementById("input-password-reset");
-    const toggleButtonReset = document.getElementById("toggle-password-reset");
-    const visibleEyeReset = toggleButtonReset.querySelector(".visible-icon");
-    const hiddenEyeReset = toggleButtonReset.querySelector(".hidden-icon");
+// Пароли для Смены пароля
+const passwordReset = document.getElementById("input-password-reset");
+const toggleButtonReset = document.getElementById("toggle-password-reset");
+const visibleEyeReset = toggleButtonReset.querySelector(".visible-icon");
+const hiddenEyeReset = toggleButtonReset.querySelector(".hidden-icon");
 
-    checkPasswordState(passwordReset, toggleButtonReset);
+checkPasswordState(passwordReset, toggleButtonReset);
 
-    // Пароли для Смены пароля (подтверждение пароля)
-    const passwordResetConf = document.getElementById(
-        "input-password-reset-confirmation"
-    );
-    const toggleButtonResetConf = document.getElementById(
-        "toggle-password-reset-confirmation"
-    );
-    const visibleEyeResetConf =
-        toggleButtonResetConf.querySelector(".visible-icon");
-    const hiddenEyeResetConf = toggleButtonResetConf.querySelector(".hidden-icon");
+// Пароли для Смены пароля (подтверждение пароля)
+const passwordResetConf = document.getElementById(
+    "input-password-reset-confirmation"
+);
+const toggleButtonResetConf = document.getElementById(
+    "toggle-password-reset-confirmation"
+);
+const visibleEyeResetConf =
+    toggleButtonResetConf.querySelector(".visible-icon");
+const hiddenEyeResetConf = toggleButtonResetConf.querySelector(".hidden-icon");
 
-    checkPasswordState(passwordResetConf, toggleButtonResetConf);
+checkPasswordState(passwordResetConf, toggleButtonResetConf);
 
-    // Обработчик для входа
-    passwordLogin.addEventListener("input", function () {
-        if (passwordLogin.value.length > 0) {
-            toggleButtonLogin.classList.add("_active");
-        } else {
-            toggleButtonLogin.classList.remove("_active");
-        }
-    });
-
-    toggleButtonLogin.addEventListener("click", function () {
-        const isPasswordType = passwordLogin.type === "password";
-        passwordLogin.type = isPasswordType ? "text" : "password";
-        visibleEyeLogin.style.display = isPasswordType
-            ? "none"
-            : "inline-block";
-        hiddenEyeLogin.style.display = !isPasswordType
-            ? "none"
-            : "inline-block";
-    });
-
-    // Обработчик для регистрации
-    passwordReg.addEventListener("input", function () {
-        if (passwordReg.value.length > 0) {
-            toggleButtonReg.classList.add("_active");
-        } else {
-            toggleButtonReg.classList.remove("_active");
-        }
-    });
-
-    toggleButtonReg.addEventListener("click", function () {
-        const isPasswordType = passwordReg.type === "password";
-        passwordReg.type = isPasswordType ? "text" : "password";
-        visibleEyeReg.style.display = isPasswordType ? "none" : "inline-block";
-        hiddenEyeReg.style.display = !isPasswordType ? "none" : "inline-block";
-    });
-
-    // Обработчик для регистрации повтор пароля
-    passwordRegConf.addEventListener("input", function () {
-        if (passwordRegConf.value.length > 0) {
-            toggleButtonRegConf.classList.add("_active");
-        } else {
-            toggleButtonRegConf.classList.remove("_active");
-        }
-    });
-
-    toggleButtonRegConf.addEventListener("click", function () {
-        const isPasswordType = passwordRegConf.type === "password";
-        passwordRegConf.type = isPasswordType ? "text" : "password";
-        visibleEyeRegConf.style.display = isPasswordType
-            ? "none"
-            : "inline-block";
-        hiddenEyeRegConf.style.display = !isPasswordType
-            ? "none"
-            : "inline-block";
-    });
-
-    // Обработчик для Смены пароля
-    passwordReset.addEventListener("input", function () {
-        if (passwordReset.value.length > 0) {
-            toggleButtonReset.classList.add("_active");
-        } else {
-            toggleButtonReset.classList.remove("_active");
-        }
-    });
-
-    toggleButtonReset.addEventListener("click", function () {
-        const isPasswordType = passwordReset.type === "password";
-        passwordReset.type = isPasswordType ? "text" : "password";
-        visibleEyeReset.style.display = isPasswordType ? "none" : "inline-block";
-        hiddenEyeReset.style.display = !isPasswordType ? "none" : "inline-block";
-    });
-
-    // Обработчик для Смены пароля (повтор пароля)
-    passwordResetConf.addEventListener("input", function () {
-        if (passwordResetConf.value.length > 0) {
-            toggleButtonResetConf.classList.add("_active");
-        } else {
-            toggleButtonResetConf.classList.remove("_active");
-        }
-    });
-
-    toggleButtonResetConf.addEventListener("click", function () {
-        const isPasswordType = passwordResetConf.type === "password";
-        passwordResetConf.type = isPasswordType ? "text" : "password";
-        visibleEyeResetConf.style.display = isPasswordType
-            ? "none"
-            : "inline-block";
-        hiddenEyeResetConf.style.display = !isPasswordType
-            ? "none"
-            : "inline-block";
-    });
-
-    // Вспомогательная функция для проверки состояния поля
-    function checkPasswordState(inputField, buttonToggle) {
-        if (inputField.value.trim().length > 0) {
-            buttonToggle.classList.add("_active");
-        } else {
-            buttonToggle.classList.remove("_active");
-        }
+// Обработчик для входа
+passwordLogin.addEventListener("input", function () {
+    if (passwordLogin.value.length > 0) {
+        toggleButtonLogin.classList.add("_active");
+    } else {
+        toggleButtonLogin.classList.remove("_active");
     }
+});
 
-    const getScrollbarWidth = () => {
-        return window.innerWidth - document.documentElement.clientWidth;
-    };
+toggleButtonLogin.addEventListener("click", function () {
+    const isPasswordType = passwordLogin.type === "password";
+    passwordLogin.type = isPasswordType ? "text" : "password";
+    visibleEyeLogin.style.display = isPasswordType ? "none" : "inline-block";
+    hiddenEyeLogin.style.display = !isPasswordType ? "none" : "inline-block";
+});
 
-    const setBodyStyle = () => {
-        const scrollbarWidth = getScrollbarWidth();
-        body.style.overflowY = "hidden";
-        body.style.position = "fixed";
-        body.style.top = `-${scrollPosition}px`;
-        body.style.width = "100%";
-        body.style.paddingRight = `${scrollbarWidth}px`;
-    };
+// Обработчик для регистрации
+passwordReg.addEventListener("input", function () {
+    if (passwordReg.value.length > 0) {
+        toggleButtonReg.classList.add("_active");
+    } else {
+        toggleButtonReg.classList.remove("_active");
+    }
+});
 
-    const setBodyStyleDefault = () => {
-        body.style.overflowY = "auto";
-        body.style.position = "";
-        body.style.top = "";
-        body.style.width = "";
-        body.style.paddingRight = "0";
-    };
+toggleButtonReg.addEventListener("click", function () {
+    const isPasswordType = passwordReg.type === "password";
+    passwordReg.type = isPasswordType ? "text" : "password";
+    visibleEyeReg.style.display = isPasswordType ? "none" : "inline-block";
+    hiddenEyeReg.style.display = !isPasswordType ? "none" : "inline-block";
+});
 
-    let scrollPosition = 0;
+// Обработчик для регистрации повтор пароля
+passwordRegConf.addEventListener("input", function () {
+    if (passwordRegConf.value.length > 0) {
+        toggleButtonRegConf.classList.add("_active");
+    } else {
+        toggleButtonRegConf.classList.remove("_active");
+    }
+});
 
-    const openModal = (modal) => {
-        if (!modal.classList.contains("_open")) {
-            scrollPosition =
-                window.scrollY || document.documentElement.scrollTop;
-            setBodyStyle();
-            modal.classList.add("_open");
-        }
-    };
+toggleButtonRegConf.addEventListener("click", function () {
+    const isPasswordType = passwordRegConf.type === "password";
+    passwordRegConf.type = isPasswordType ? "text" : "password";
+    visibleEyeRegConf.style.display = isPasswordType ? "none" : "inline-block";
+    hiddenEyeRegConf.style.display = !isPasswordType ? "none" : "inline-block";
+});
 
-    const closeModal = (modal) => {
-        modal.classList.remove("_open");
-        setBodyStyleDefault();
-        window.scrollTo(0, scrollPosition);
-    };
+// Обработчик для Смены пароля
+passwordReset.addEventListener("input", function () {
+    if (passwordReset.value.length > 0) {
+        toggleButtonReset.classList.add("_active");
+    } else {
+        toggleButtonReset.classList.remove("_active");
+    }
+});
 
-    const signInLink = document.querySelector('.switch-to-signin');
-    const signUpLink = document.querySelector('.switch-to-signup');
-    const formsContainer = document.querySelector('.modal__body'); // Родительский контейнер обоих форм
+toggleButtonReset.addEventListener("click", function () {
+    const isPasswordType = passwordReset.type === "password";
+    passwordReset.type = isPasswordType ? "text" : "password";
+    visibleEyeReset.style.display = isPasswordType ? "none" : "inline-block";
+    hiddenEyeReset.style.display = !isPasswordType ? "none" : "inline-block";
+});
 
-    // Переключение на форму регистрации
-    signUpLink.addEventListener('click', function(event) {
+// Обработчик для Смены пароля (повтор пароля)
+passwordResetConf.addEventListener("input", function () {
+    if (passwordResetConf.value.length > 0) {
+        toggleButtonResetConf.classList.add("_active");
+    } else {
+        toggleButtonResetConf.classList.remove("_active");
+    }
+});
+
+toggleButtonResetConf.addEventListener("click", function () {
+    const isPasswordType = passwordResetConf.type === "password";
+    passwordResetConf.type = isPasswordType ? "text" : "password";
+    visibleEyeResetConf.style.display = isPasswordType
+        ? "none"
+        : "inline-block";
+    hiddenEyeResetConf.style.display = !isPasswordType
+        ? "none"
+        : "inline-block";
+});
+
+// Вспомогательная функция для проверки состояния поля
+function checkPasswordState(inputField, buttonToggle) {
+    if (inputField.value.trim().length > 0) {
+        buttonToggle.classList.add("_active");
+    } else {
+        buttonToggle.classList.remove("_active");
+    }
+}
+
+const getScrollbarWidth = () => {
+    return window.innerWidth - document.documentElement.clientWidth;
+};
+
+const setBodyStyle = () => {
+    const scrollbarWidth = getScrollbarWidth();
+    body.style.overflowY = "hidden";
+    body.style.position = "fixed";
+    body.style.top = `-${scrollPosition}px`;
+    body.style.width = "100%";
+    body.style.paddingRight = `${scrollbarWidth}px`;
+};
+
+const setBodyStyleDefault = () => {
+    body.style.overflowY = "auto";
+    body.style.position = "";
+    body.style.top = "";
+    body.style.width = "";
+    body.style.paddingRight = "0";
+};
+
+let scrollPosition = 0;
+
+const openModal = (modal) => {
+    if (!modal.classList.contains("_open")) {
+        scrollPosition = window.scrollY || document.documentElement.scrollTop;
+        setBodyStyle();
+        modal.classList.add("_open");
+    }
+};
+
+const closeModal = (modal) => {
+    modal.classList.remove("_open");
+    setBodyStyleDefault();
+    window.scrollTo(0, scrollPosition);
+};
+
+const signInLinks = document.querySelectorAll(".switch-to-signin");
+const signUpLink = document.querySelector(".switch-to-signup");
+const formsContainer = document.querySelector(".modal__body"); // Родительский контейнер обоих форм
+
+// Переключение на форму регистрации
+signUpLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    formsContainer.querySelector(".form-signin").style.display = "none"; // Прячем форму входа
+    formsContainer.querySelector(".form-signup").style.display = "block"; // Показываем форму регистрации
+});
+
+// Переключение обратно на форму входа
+
+signInLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
         event.preventDefault();
-        formsContainer.querySelector('.form-signin').style.display = 'none'; // Прячем форму входа
-        formsContainer.querySelector('.form-signup').style.display = 'block'; // Показываем форму регистрации
+        formsContainer.querySelector(".form-signin").style.display = "block"; // Показываем форму входа
+        formsContainer.querySelector(".form-signup").style.display = "none"; // Прячем форму регистрации
+        formsContainer.querySelector(".form-verify-email").style.display =
+            "none";
+        formsContainer.querySelector(
+            ".form-password-reset-link"
+        ).style.display = "none";
+        formsContainer.querySelector(".form-password-reset").style.display =
+            "none";
     });
+});
 
-    // Переключение обратно на форму входа
-    signInLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        formsContainer.querySelector('.form-signin').style.display = 'block'; // Показываем форму входа
-        formsContainer.querySelector('.form-signup').style.display = 'none'; // Прячем форму регистрации
-    });
-
-
-
-    if (modals.length > 0) {
-        modalCallBtns.forEach((btn) => {
-            btn.addEventListener("click", (e) => {
-                e.preventDefault();
-                const modalName = btn.dataset.modalLink;
-                const targetModal = document.querySelector(
-                    `[data-modal="${modalName}"]`
+if (modals.length > 0) {
+    modalCallBtns.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            const modalName = btn.dataset.modalLink;
+            const targetModal = document.querySelector(
+                `[data-modal="${modalName}"]`
+            );
+            if (targetModal) {
+                openModal(targetModal);
+            } else {
+                console.error(
+                    `Модальное окно с data-modal="${modalName}" не найдено!`
                 );
-                if (targetModal) {
-                    openModal(targetModal);
-                } else {
-                    console.error(
-                        `Модальное окно с data-modal="${modalName}" не найдено!`
-                    );
-                }
-            });
+            }
         });
+    });
 
-        modals.forEach((modalWindow) => {
-            modalWindow.addEventListener("click", (e) => {
-                if (e.target.dataset.close) {
-                    closeModal(modalWindow);
-                }
-            });
+    modals.forEach((modalWindow) => {
+        modalWindow.addEventListener("click", (e) => {
+            if (e.target.dataset.close) {
+                closeModal(modalWindow);
+            }
         });
-    }
-    console.log("Modal is ready...");
+    });
+}
+console.log("Modal is ready...");
