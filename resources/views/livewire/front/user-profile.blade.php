3a83a1e4b987d0ec609b7c75c21a9a8ac1ac2ca6
johanius <info@kevtek.ru>
2025-10-03 19:22:09 +0300

    add accountPage

--- resources/views/livewire/front/user-profile.blade.php
+++ resources/views/livewire/front/user-profile.blade.php
@@ -1,7 +1,7 @@
 <div class="account-profile__wrapper">
     <nav class="account-profile__control">
         <p class="profile-full-name">{{ $user->last_name }} {{ $user->name }} {{ $user->patronymic ?? '' }}</p>
-
+        <p class="profile-user-status">{{ $user->group->name }}</p> <span>{{ $user->group->discount }}</span>
         <ul class="account-profile__control-list">
             <li class="account-profile__control-item">
                 <button data-target="profile-card" wire:click="selectPanel('profile')"
