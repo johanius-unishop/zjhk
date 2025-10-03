7c5ff71cb94c05f5da73ffc1475ba3ade98c1912
johanius <info@kevtek.ru>
2025-10-03 15:52:12 +0300

    add accountPage

--- resources/views/front/profile/index.blade.php
+++ resources/views/front/profile/index.blade.php
@@ -25,6 +25,17 @@
 @stop
 
 @section('scripts')
-    <script>https://cdnjs.cloudflare.com/ajax/libs/cleave.js/1.6.0/cleave.min.js</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cleave.js/1.6.0/cleave.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cleave.js/1.6.0/addons/cleave-phone.ru.js"></script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var phoneNumberElement = document.getElementById('profile-phone');
+            new Cleave(phoneNumberElement, {
+                phone: true,
+                phoneRegionCode: 'RU'
+            });
+        });
+    </script>
 @endsection
 
