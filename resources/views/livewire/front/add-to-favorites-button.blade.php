69aeb20f86a93b6652febd4f5f331f74d18f97da
johanius <info@kevtek.ru>
2025-10-06 17:11:50 +0300

    favorite products

--- resources/views/livewire/front/add-to-favorites-button.blade.php
+++ resources/views/livewire/front/add-to-favorites-button.blade.php
@@ -1,13 +1,13 @@
 <div class="product-desc__cart-actions" wire:ignore.self>
-    <button wire:click="toggleFavorite" class="product-page__label-btn">
-        @if ($isInFavorites)
-            Удалить из избранных
-        @else
-            <img class="product-page__label" src="{{ asset('images/icons/label-gray.svg') }}" alt="избранное">
-            В избранное
-        @endif
+    <!-- Button for adding or removing from favorites -->
+    <button wire:click="toggleFavorite" class="product-page__label-btn {{ $isInFavorites ? '_active' : '' }}">
+        <img class="product-page__label" src="{{ asset('images/icons/label-gray.svg') }}" alt="избранное">
+        В избранное
     </button>
-    <button><img src="{{ asset('images/icons/share.svg') }}" alt="поделиться">
+
+    <!-- Share button -->
+    <button>
+        <img src="{{ asset('images/icons/share.svg') }}" alt="поделиться">
         Поделиться
     </button>
 </div>
