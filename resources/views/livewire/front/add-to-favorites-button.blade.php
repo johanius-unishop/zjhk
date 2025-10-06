ed1f3816502ef9cf29d0cc837f8d59937ac8d738
johanius <info@kevtek.ru>
2025-10-06 17:33:41 +0300

    favorite products

--- resources/views/livewire/front/add-to-favorites-button.blade.php
+++ resources/views/livewire/front/add-to-favorites-button.blade.php
@@ -1,6 +1,6 @@
 <div class="product-desc__cart-actions" wire:ignore.self>
     <!-- Button for adding or removing from favorites -->
-    <button wire:click="toggleFavorite" class="product-page__label-btn {{ $isInFavorites ? '_active' : '' }}">
+    <button wire:click="toggleFavorite" class="product-page__label-btn">
         <img class="product-page__label"
             src="
         {{ $isInFavorites ? asset('images/icons/label-blue.svg') : asset('images/icons/label-gray.svg') }}
