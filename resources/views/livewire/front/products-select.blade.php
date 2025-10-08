19e1a9441eae0dda6443eb4447bbc0b046b73394
johanius <info@kevtek.ru>
2025-10-08 17:30:39 +0300

    favorite products

--- resources/views/livewire/front/products-select.blade.php
+++ resources/views/livewire/front/products-select.blade.php
@@ -111,25 +111,6 @@
         <div class="product-page__pagination">
             <div class="product-page__pagination-content">
                 {{ $products->links('vendor.pagination.custom') }}
-                <div class="product-page__page-size" data-page-size>
-                    <p class="product-page__page-select">
-                        <span>Показывать по </span>
-                        <button wire:click="toggleMenu" class="product-page__change-btn">
-                            <span id="page-count">{{ $perPage }}</span>
-                            <img src="{{ asset('images/icons/tick-black.svg') }}" alt="список страниц">
-                        </button>
-                    </p>
-                    <ul class="product-page__page-count {{ $menuIsOpen === true ? '_active' : 'hidden' }}"
-                        data-select-page>
-                        @foreach ($perPageOptions as $option)
-                            <li>
-                                <button wire:click="changePerPage({{ $option }})" wire:loading.attr="disabled">
-                                    {{ $option }}
-                                </button>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
             </div>
         </div>
     </div>
