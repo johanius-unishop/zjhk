b5b1c66c5a20e9bd082165d9eedffb1e9b7a3ea4
johanius <info@kevtek.ru>
2025-10-09 14:54:39 +0300

    favorite products

--- resources/views/livewire/front/products-select.blade.php
+++ resources/views/livewire/front/products-select.blade.php
@@ -70,7 +70,7 @@
                                         alt="{{ $product_item->getAltAttribute() }}" loading="lazy">
                                 @endif
                             </a>
-
+                            @livewire('front.add-to-favorites-button', ['contentType' => 'productSelect', 'productId' => $product_item->id])
                         </div>
                         <div data-layout
                             class="product-page__title-container {{ $layoutType === 'card' ? 'card-layout' : 'list-layout' }}">
@@ -136,3 +136,4 @@
         </div>
     </div>
 </div>
+
