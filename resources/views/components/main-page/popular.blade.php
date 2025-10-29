3f006545eaf22eb1cc61a20d1a4eb057274e8240
johanius <info@kevtek.ru>
2025-10-29 22:58:15 +0300

    popular products

--- resources/views/components/main-page/popular.blade.php
+++ resources/views/components/main-page/popular.blade.php
@@ -1,104 +1,24 @@
 <section class="popular">
-            <div class="popular__container container">
-               <div class="popular__wrapper">
-                  <h2 class="popular__title">
-                     Популярные товары
-                  </h2>
-                  <div class="popular__grid hide-subsequent-rows">
-                    @foreach ($popularProductsWithDetails as $popularProduct)
-                     <a href="#">
+    <div class="popular__container container">
+        <div class="popular__wrapper">
+            <h2 class="popular__title">
+                Популярные товары
+            </h2>
+            <div class="popular__grid hide-subsequent-rows">
+                @foreach ($popularProductsWithDetails as $popularProduct)
+                    <a href="{{ route('product.show', ['slug' => $popularProduct->product->slug]) }}">
                         <div class="popular__item-wrapper">
-                           <div>
-                              <img src="/images/products/photo-2.png" alt="товар">
-                           </div>
-                           <div>
-                              <h5>HSB-006-SE-BK-1L-M25</h5>
-                              <p>{{ $popularProduct->product->article }}</p>
-                           </div>
+                            <div>
+                                <img src="/images/products/photo-2.png" alt="товар">
+                            </div>
+                            <div>
+                                <h5>{{ $popularProduct->product->name }}<</h5>
+                                <p>{{ $popularProduct->product->article }}</p>
+                            </div>
                         </div>
-                     </a>
-                     @endforeach
-                     <a href="#">
-                        <div class="popular__item-wrapper">
-                           <div>
-                              <img src="/images/products/photo-1.png" alt="товар">
-                           </div>
-                           <div>
-                              <h5>HE-006-TE-CCT-1L-2PG16</h5>
-                              <p>K000000296</p>
-                           </div>
-                        </div>
-                     </a>
-                     <a href="#">
-                        <div class="popular__item-wrapper">
-                           <div>
-                              <img src="/images/products/photo-3.png" alt="товар">
-                           </div>
-                           <div>
-                              <h5>HA-003-SE-BK-1L-MCV-M20</h5>
-                              <p>K000000006</p>
-                           </div>
-                        </div>
-                     </a>
-                     <a href="#">
-                        <div class="popular__item-wrapper">
-                           <div>
-                              <img src="/images/products/photo-14.png" alt="товар">
-                           </div>
-                           <div>
-                              <h5>HK-008/0-SE-SF-2L-2PG21</h5>
-                              <p>K000003531</p>
-                           </div>
-                        </div>
-                     </a>
-                     <a href="#">
-                        <div class="popular__item-wrapper">
-                           <div>
-                              <img src="/images/products/photo-5.png" alt="товар">
-                           </div>
-                           <div>
-                              <h5> HA-003-F-BK-1L
-                              </h5>
-                              <p>PK00000007</p>
-                           </div>
-                        </div>
-                     </a>
-                     <a href="#">
-                        <div class="popular__item-wrapper">
-                           <div>
-                              <img src="/images/products/photo-6.png" alt="товар">
-                           </div>
-                           <div>
-                              <h5> HE-006-F-SF-1L-M20
-                              </h5>
-                              <p>PK00000181</p>
-                           </div>
-                        </div>
-                     </a>
-                     <a href="#">
-                        <div class="popular__item-wrapper">
-                           <div>
-                              <img src="/images/products/photo-7.png" alt="товар">
-                           </div>
-                           <div>
-                              <h5> HSB-006-F-BK-1L
-                              </h5>
-                              <p>PK00002441</p>
-                           </div>
-                        </div>
-                     </a>
-                     <a href="#">
-                        <div class="popular__item-wrapper">
-                           <div>
-                              <img src="/images/products/photo-8.png" alt="товар">
-                           </div>
-                           <div>
-                              <h5>HK-004/0-M-SE-2B-M25</h5>
-                              <p>PK00002890</p>
-                           </div>
-                        </div>
-                     </a>
-                  </div>
-               </div>
+                    </a>
+                @endforeach
             </div>
-         </section>
+        </div>
+    </div>
+</section>
