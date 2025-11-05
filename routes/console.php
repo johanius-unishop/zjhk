2844737a1534240dfe0ca81fcae0b468f4410107
johanius <info@kevtek.ru>
2025-11-05 17:31:19 +0300

    popular products

--- routes/console.php
+++ routes/console.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('fetch:exchange-rates')->dailyAt('05:00')->timezone('Europe/Moscow');
 Schedule::command('products:update-simple-products-stock-from-1c')->everyFiveMinutes();
-Schedule::command('products:update-popular-products-from-1c')->everFiveMinutes();
+Schedule::command('products:update-popular-products-from-1c')->everyTwentyMinutes();
 Schedule::command('queue:work --stop-when-empty --queue=high,default')->everyMinute();
 Schedule::command('delete:inactive-users')->daily();
