7c9806cb188d694047fc93042a83ea4b7fc58420
johanius <info@kevtek.ru>
2025-11-07 18:06:11 +0300

    documentation table

--- resources/views/admin/content/index.blade.php
+++ resources/views/admin/content/index.blade.php
@@ -6,80 +6,113 @@
 
 @section('title', 'Контент')
 @section('content_header')
-<h1>Контент</h1>
+    <h1>Контент</h1>
 @stop
 @section('content')
 
-<!--  .row -->
-<div class="row">
-    <div class="col-lg-3 col-6">
-        <!-- small card -->
-        <div class="small-box bg-info">
-            <div class="inner">
-                <h3>{{ $article_count }}</h3>
-                <p>Статьи</p>
+    <!--  .row -->
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h4>{{ $article_count }}</h4>
+                    <p>Статьи</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-newspaper"></i>
+                </div>
+                <a href="{{ route('admin.article.index') }}" class="small-box-footer">
+                    Перейти <i class="fas fa-arrow-circle-right"></i>
+                </a>
             </div>
-            <div class="icon">
-                <i class="fas fa-newspaper"></i>
-            </div>
-            <a href="{{ route('admin.article.index') }}" class="small-box-footer">
-                Перейти <i class="fas fa-arrow-circle-right"></i>
-            </a>
         </div>
-    </div>
-    <div class="col-lg-3 col-6">
-        <!-- small card -->
-        <div class="small-box bg-info">
-            <div class="inner">
-                <h3>{{ $news_count }}</h3>
-                <p>Новости</p>
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h4>{{ $news_count }}</h4>
+                    <p>Новости</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-newspaper"></i>
+                </div>
+                <a href="{{ route('admin.news.index') }}" class="small-box-footer">
+                    Перейти <i class="fas fa-arrow-circle-right"></i>
+                </a>
             </div>
-            <div class="icon">
-                <i class="fas fa-newspaper"></i>
-            </div>
-            <a href="{{ route('admin.news.index') }}" class="small-box-footer">
-                Перейти <i class="fas fa-arrow-circle-right"></i>
-            </a>
         </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small card -->
-        <div class="small-box bg-warning">
-            <div class="inner">
-                <h3> {{ $page_count }}</h3>
-                <p>Статические страницы</p>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h4> {{ $page_count }}</h4>
+                    <p>Статические страницы</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-newspaper"></i>
+                </div>
+                <a href="{{ route('admin.page.index') }}" class="small-box-footer">
+                    Перейти <i class="fas fa-arrow-circle-right"></i>
+                </a>
             </div>
-            <div class="icon">
-                <i class="fas fa-newspaper"></i>
-            </div>
-            <a href="{{ route('admin.page.index') }}" class="small-box-footer">
-                Перейти <i class="fas fa-arrow-circle-right"></i>
-            </a>
         </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small card -->
-        <div class="small-box bg-warning">
-            <div class="inner">
-                <h3> {{ $faq_count }}</h3>
-                <p>Faq (ЧаВо)</p>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h4> {{ $faq_count }}</h4>
+                    <p>Faq (ЧаВо)</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-newspaper"></i>
+                </div>
+                <a href="{{ route('admin.faq.index') }}" class="small-box-footer">
+                    Перейти <i class="fas fa-arrow-circle-right"></i>
+                </a>
             </div>
-            <div class="icon">
-                <i class="fas fa-newspaper"></i>
-            </div>
-            <a href="{{ route('admin.faq.index') }}" class="small-box-footer">
-                Перейти <i class="fas fa-arrow-circle-right"></i>
-            </a>
         </div>
+
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h4>Тип документации</h4>
+                    <p>Каталог, обзорная информация и т.д.</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-newspaper"></i>
+                </div>
+                <a href="{{ route('admin.faq.index') }}" class="small-box-footer">
+                    Перейти <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h4> {{ $faq_count }}</h4>
+                    <p>Faq (ЧаВо)</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-newspaper"></i>
+                </div>
+                <a href="{{ route('admin.faq.index') }}" class="small-box-footer">
+                    Перейти <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
     </div>
-</div>
-<!-- /.row -->
+    <!-- /.row -->
 @endsection
 
 @section('scripts')
-@parent
+    @parent
 
 
 @endsection
