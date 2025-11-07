4abfd55cb3ce3ed9fc26cab213634b94a34f82b6
johanius <info@kevtek.ru>
2025-11-07 19:07:23 +0300

    documentation table

--- app/Livewire/DocumentationTypeTable.php
+++ app/Livewire/DocumentationTypeTable.php
@@ -2,22 +2,15 @@
 
 namespace App\Livewire;
 
-use App\Models\Article;
-use Illuminate\Support\Carbon;
+use App\Models\DocumentationType;
 use Illuminate\Database\Eloquent\Builder;
 use PowerComponents\LivewirePowerGrid\Button;
 use PowerComponents\LivewirePowerGrid\Column;
 
-use PowerComponents\LivewirePowerGrid\Facades\Filter;
-use PowerComponents\LivewirePowerGrid\Footer;
-use PowerComponents\LivewirePowerGrid\Header;
 use PowerComponents\LivewirePowerGrid\Facades\PowerGrid;
 use PowerComponents\LivewirePowerGrid\PowerGridFields;
 use PowerComponents\LivewirePowerGrid\PowerGridComponent;
-use PowerComponents\LivewirePowerGrid\Traits\WithExport;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
-use PowerComponents\LivewirePowerGrid\Facades\Rule;
-use Illuminate\Support\Facades\Log;
 
 final class DocumentationTypeTable extends PowerGridComponent
 {
@@ -38,7 +31,7 @@ final class DocumentationTypeTable extends PowerGridComponent
 
     public function datasource(): Builder
     {
-        return Article::query()->orderBy('order_column');
+        return DocumentationType::query();
     }
 
     public function relationSearch(): array
@@ -51,7 +44,6 @@ final class DocumentationTypeTable extends PowerGridComponent
         return PowerGrid::fields()
             ->add('id')
             ->add('name')
-            ->add('published', closure: fn($item) => $item->published ? '✅' : '❌')
             ->add('created_at');
     }
 
@@ -62,63 +54,16 @@ final class DocumentationTypeTable extends PowerGridComponent
             Column::make('Наименование', 'name')
                 ->sortable()
                 ->searchable(),
-            Column::make('Опубликовано', 'published'),
             Column::make('Создано', field: 'created_at'),
-
-
             Column::action('Действия'),
         ];
     }
 
-    public function filters(): array
-    {
-        return [
-            Filter::boolean('published')
-                ->label('✅', '❌'),
-
-
-        ];
-    }
-
-
-    #[\Livewire\Attributes\On('up')]
-    public function property_up($rowId): void
-    {
-        try {
-            $property = Article::findOrFail($rowId);
-            $property->moveOrderUp();
-            $this->dispatch('toast', message: 'Запись успешно поднята вверх.', notify: 'success');
-        }
-        catch (\Throwable $th) {
-            Log::info('Ошибка  выполнения скрипта: ' . $th->getMessage() . ' .');
-            $this->dispatch('toast', message: ' Не удалось поднять  запись.' . $th->getMessage(), notify: 'error');
-            throw $th;
-        }
-        $this->dispatch('$refresh');
-    }
-
-
-    #[\Livewire\Attributes\On(event: 'down')]
-    public function property_down($rowId): void
-    {
-        try {
-            $property = Article::findOrFail($rowId);
-            $property->moveOrderDown();
-            $this->dispatch('toast', message: 'Запись успешно опущена вниз.', notify: 'success');
-        }
-        catch (\Throwable $th) {
-            Log::info($property->name . 'Ошибка  выполнения скрипта: ' . $th->getMessage() . ' .');
-            $this->dispatch('toast', message: ' Не удалось опустить  запись.' . $th->getMessage(), notify: 'error');
-            throw $th;
-        }
-        $this->dispatch('$refresh');
-
-    }
-    #[\Livewire\Attributes\On('post_delete')]
-    public function post_delete($rowId): void
+    #[\Livewire\Attributes\On('doc_type_delete')]
+    public function doc_type_delete($rowId): void
     {
         $this->delete_id = $rowId;
-        $this->confirm('Вы действительно хотите удалить эту запись?', [
+        $this->confirm('Вы действительно хотите удалить этот тип документации?', [
             'onConfirmed' => 'confirmed',
             'showCancelButton' => true,
             'cancelButtonText' => 'Нет',
@@ -128,27 +73,19 @@ final class DocumentationTypeTable extends PowerGridComponent
     #[\Livewire\Attributes\On('confirmed')]
     public function confirmed()
     {
-        $deleted_record = Article::where('id', $this->delete_id)->firstOrFail();
+        $deleted_record = DocumentationType::where('id', $this->delete_id)->firstOrFail();
         $deleted_record->delete();
         $this->dispatch('toast', message: 'Запись удалена.', notify: 'success');
     }
 
-    public function actions(Article $row): array
+    public function actions(DocumentationType $row): array
     {
         return [
             Button::add('view')
                 ->slot('<i class="fas fa-edit"></i>')
                 ->class('btn btn-primary')
-                ->route('admin.article.edit', ['article' => $row->id]),
-            Button::add('up')
-                ->slot('<i class="fas fa-arrow-up"></i>')
-                ->class('btn btn-success')
-                ->dispatch('up', ['rowId' => $row->id]),
-            Button::add('down')
-                ->slot('<i class="fas fa-arrow-down"></i>')
-                ->class('btn btn-success')
-                ->dispatch('down', ['rowId' => $row->id]),
-            Button::add('delivery')
+                ->route('admin.documentation-type.edit', ['article' => $row->id]),
+            Button::add('delete')
                 ->slot('<i class="fas fa-trash"></i>')
                 ->class('btn btn-danger')
                 ->dispatch('post_delete', ['rowId' => $row->id]),
