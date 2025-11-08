a2869c2c20cf69b422a4446845a2c3ae69d5c44d
johanius <info@kevtek.ru>
2025-11-08 22:22:15 +0300

    documentation table

--- app/Http/Requests/Admin/StoreDocumentationRequest.php
+++ app/Http/Requests/Admin/StoreDocumentationRequest.php
@@ -26,4 +26,22 @@ class StoreDocumentationRequest extends FormRequest
             'text'       => 'required|min:2|max:200',
         ];
     }
+
+    /**
+     * Customize error messages for each rule.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'title.required'      => 'Название документа обязательно для заполнения.',
+            'title.min'           => 'Минимальная длина поля "Название документа" составляет 2 символа.',
+            'title.max'           => 'Максимальная длина поля "Название документа" составляет 50 символов.',
+
+            'text.required'      => 'Описание документа обязательно для заполнения.',
+            'text.min'           => 'Минимальная длина поля "Описание документа" составляет 2 символа.',
+            'text.max'           => 'Максимальная длина поля "Описание документа" составляет 200 символов.',
+        ];
+    }
 }
