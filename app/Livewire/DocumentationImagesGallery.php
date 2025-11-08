c10ab43d72b23b8c8ffe386b806896e93a92577a
johanius <info@kevtek.ru>
2025-11-08 20:40:10 +0300

    documentation table

---
+++ app/Livewire/DocumentationImagesGallery.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Documentation;
+use Livewire\Component;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Livewire\WithFileUploads;
+
+class DocumentationImagesGallery extends Component
+{
+    use WithFileUploads;
+    use LivewireAlert;
+    public $photo;
+    public $image;
+    public $record;
+
+    public function mount(Documentation $record)
+    {
+        $this->record = $record;
+        $this->loadImage();
+    }
+
+    public function loadImage()
+    {
+        // Перезагрузка модели $this->record
+        $this->record = $this->record->fresh();
+        $this->image = $this->record->getMedia('images')->first();
+
+        $this->dispatch('documentation-images-gallery','$refresh');
+
+    }
+
+    public function delete($media_id)
+    {
+        $media = Media::find($media_id);
+        $media->delete();
+        $this->dispatch('toast', message: 'Изображение документа удалено!', notify: 'error');
+        $this->loadImage();
+
+    }
+
+    public function uploadFiles()
+    {
+        $this->validate([
+            'photo' => 'mimes:jpeg,png,jpg|max:10240',
+        ]);
+
+        if ($this->photo) {
+            $this->record->addMedia($this->photo)->toMediaCollection('images');
+            $this->reset('photo');
+            $this->loadImage();
+            $this->dispatch('toast', message: 'Изображение для документа загружено.', notify: 'success');
+        }
+    }
+
+    public function download(Media $mediaItem)
+    {
+        return response()->download($mediaItem->getPath(), $mediaItem->file_name);
+    }
+
+    public function render()
+    {
+        return view('livewire.documentation-images-gallery');
+    }
+}
