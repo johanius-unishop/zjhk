68faf3333ac5ff78560f712f752b3e6f0abbe29a
johanius <info@kevtek.ru>
2025-12-04 00:01:04 +0300

    docs wrapper

--- public/js/choices.js
+++ public/js/choices.js
@@ -28,7 +28,7 @@ function initChoices() {
             const fieldName = event.target.name;
             const selectedValue = event.target.value;
 
-            Livewire.dispatch('update-value', fieldName, selectedValue);
+            Livewire.dispatch('update-value', fieldName);
         });
     });
 }
