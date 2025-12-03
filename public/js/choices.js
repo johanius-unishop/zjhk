69fdbf9ad9483bb59409e8df9be903e0487668a2
johanius <info@kevtek.ru>
2025-12-03 22:31:58 +0300

    docs wrapper

--- public/js/choices.js
+++ public/js/choices.js
@@ -21,6 +21,8 @@ function initChoices() {
 }
 
 function refreshChoices() {
+
+    const selectElements = document.querySelectorAll(".filter__select");
     selectElements.forEach((selectElement) => {
         if (selectElement._choices) {
             selectElement._choices.refresh(true, false); // Читаем опции и не выбираем автоматически первый элемент
