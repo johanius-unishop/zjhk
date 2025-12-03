92199719aec86d8781659beb2d75d2a8c59e2fa1
johanius <info@kevtek.ru>
2025-12-03 22:49:27 +0300

    docs wrapper

--- public/js/choices.js
+++ public/js/choices.js
@@ -11,17 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
 function initChoices() {
     const selectElements = document.querySelectorAll(".filter__select");
 
-    selectElements.forEach((el) => {
-        const choices = new Choices(el, {
+    selectElements.forEach((selectElement) => {
+        // Текущее выбранное значение
+        const currentValue = selectElement.value;
+
+        // Если экземпляр уже существует, уничтожаем его
+        if (selectElement._choices) {
+            selectElement._choices.destroy();
+        }
+
+        // Создаём новый экземпляр Choices.js
+        selectElement._choices = new Choices(selectElement, {
             searchEnabled: false,
             itemSelectText: false,
             position: "bottom",
         });
-    });
-    selectElements.forEach((selectElement) => {
-        if (selectElement._choices) {
-            selectElement._choices.refresh(true, false); // Читаем опции и не выбираем автоматически первый элемент
-        }
+
+        // Устанавливаем выбранное значение
+        selectElement._choices.setChoiceByValue(currentValue);
     });
 }
 
