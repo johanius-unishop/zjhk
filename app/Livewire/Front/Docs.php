528ec5b9914c235c44a1b4c035bdfc2f39af46d2
johanius <info@kevtek.ru>
2025-12-04 13:23:10 +0300

    docs wrapper

--- app/Livewire/Front/Docs.php
+++ app/Livewire/Front/Docs.php
@@ -83,7 +83,7 @@ class Docs extends Component
     }
 
     #[On('update-value')]
-    public function updateValue($data)
+    public function updateValue(array $data)
     {
         // Данные приходят как ассоциативный массив
         $name = $data['name']; // Извлекаем имя поля
