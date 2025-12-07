6da8247319c0b7c845d71b6e5ba88abe0ac38cf8
johanius <info@kevtek.ru>
2025-12-07 22:43:05 +0300

    brands section on homepage

--- app/Livewire/Front/Vendors.php
+++ app/Livewire/Front/Vendors.php
@@ -4,19 +4,14 @@ namespace App\Livewire\Front;
 
 use Livewire\Component;
 use Livewire\WithPagination;
-use App\Models\Documentation;
 use App\Models\Vendor;
-use App\Models\DocumentationType;
-use Livewire\Attributes\On;
-
 
 class Vendors extends Component
 {
     use WithPagination;
 
     public $acceptsWebP;
-    public $selectedVendor = null;
-    public $selectedType = null;
+
     public $perPage = 12; // Кол-во товаров на странице
     public $perPageOptions = [12, 24, 36]; // Опции кол-ва товаров
 
@@ -43,55 +38,14 @@ class Vendors extends Component
 
     public function render()
     {
-        $usedVendorIds = Documentation::select('vendor_id')->distinct()->pluck('vendor_id');
-        $vendors = Vendor::whereIn('id', $usedVendorIds)
-            ->orderBy('name')
-            ->get();
-
-        $usedTypeIds = Documentation::select('type_id')->distinct()->pluck('type_id');
-        $types = DocumentationType::whereIn('id', $usedTypeIds)
-            ->orderBy('name')
-            ->get();
-
-        // Базовый запрос к продуктам
-        $query = Documentation::query()->orderBy('order_column', 'asc');
-
-        // Применяем фильтр по бренду, если выбрано какое-то значение
-
-        if (!is_null($this->selectedVendor)) {
-            $query->where('vendor_id', $this->selectedVendor); // Предположим, что vendor_id существует в таблице documentation
-        }
-
-        // Аналогично применяем фильтр по типу документации
-        if (!is_null($this->selectedType)) {
-            $query->where('type_id', $this->selectedType); // Предположим, что type_id существует в таблице documentation
-        }
-
+        // Базовый запрос к брендам
+        $query = Vendor::published()->orderBy('order_column', 'asc');
 
         // Выполняем пагинацию
-        $docs = $query->with('media')->paginate($this->perPage)->withQueryString();
+        $vendors = $query->with('media')->paginate($this->perPage)->withQueryString();
 
-        $this->dispatch('update-docs');
+        $this->dispatch('update-vendors');
 
-        return view('livewire.front.docs', compact('docs', 'vendors', 'types'));
-    }
-
-    #[On('update-value')]
-    public function updateValue(array $payload)
-    {
-        if (isset($payload[0]) && isset($payload[1])) {
-            $fieldName = $payload[0];
-            $selectedValue = $payload[1];
-
-            switch ($fieldName) {
-                case 'vendor':
-                    $this->selectedVendor = empty($selectedValue) ? null : $selectedValue;
-                    break;
-                case 'type':
-                    $this->selectedType = empty($selectedValue) ? null : $selectedValue;
-                    break;
-            }
-        }
-        $this->resetPage();
+        return view('livewire.front.vendors', compact('vendors'));
     }
 }
