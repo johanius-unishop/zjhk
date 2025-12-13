b4937852ab8c6eee010fa746f2ed7704aabcc479
johanius <info@kevtek.ru>
2025-12-13 16:36:50 +0300

    articles section on homepage

---
+++ resources/views/front/static-page/articles/article-2.blade.php
@@ -0,0 +1,227 @@
+<main>
+    <section class="article-page">
+        <div class="article-page__container container">
+            @include('components.article-page.breadcrumbs')
+            <div class="article-page__content">
+                <h2 class="article-page__title">Типы клемм используемые в контактных вставках</h2>
+                <h2 class="article-page__title">Винтовые клеммы</h2>
+                <div class="element-container">
+                    <div class="element-column-50">
+                        <div class="element-column-wrapper">
+                            <div class="element-cell element-cell-image">
+                                <img decoding="async" width="500" height="500"
+                                    src="https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal.jpg"
+                                    class="attachment-large size-large wp-image-5335"
+                                    alt="Винтовые клеммы в прямоугольных промышленных разъемах"
+                                    srcset="https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal.jpg 500w, https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal-300x300.jpg 300w, https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal-100x100.jpg 100w, https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal-64x64.jpg 64w, https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal-150x150.jpg 150w"
+                                    sizes="(max-width: 500px) 100vw, 500px" title="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element-column-50">
+                        <div>
+                            <div>
+                                <h5 class="article-page__h5">Преимущества</h5>
+                            </div>
+                            <div>
+                                <ul class="article-page__ul-disc">
+                                    <li>Простое и интуитивно понятное соединение</li>
+                                    <li>Монтаж можно произвести на месте без без каких-либо специальных инструментов,
+                                        достаточно иметь отверткуМонтаж можно произвести на месте без без каких-либо
+                                        специальных инструментов, достаточно иметь отвертку</li>
+                                    <li>Может использоваться для многожильных проводов клеммы</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p><strong>Винтовые клеммы</strong> являются распространенным типом клеммных соединений в промышленных
+                    прямоугольных разъемах. Они широко используются в электрических цепях для соединения проводов и
+                    кабелей. Винтовые клеммы обеспечивают надежное соединение, которое может выдерживать высокие токи и
+                    напряжения. Кроме того, они легко устанавливаются и могут быть быстро заменены при необходимости.
+                    Недостатком винтовых клемм является то, что они могут требовать больше времени и усилий для
+                    установки, чем другие типы клеммных соединений, таких как пружинные клеммы. Однако, в целом,
+                    винтовые клеммы являются надежным и простым в использовании решением для соединения проводов в
+                    электрических цепях.</p>
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+
+                <div class="article-page__img-container">
+                    <div class="article-page__img-item full-width">
+                        @if ($acceptsWebP)
+                            <img src="{{ asset('images/content/vendors/zjhk/zjhk_3.webp') }}" alt="Соединители ZJHK">
+                        @else
+                            <img src="{{ asset('images/content/vendors/zjhk/zjhk_3.jpg') }}" alt="Соединители ZJHK">
+                        @endif
+                    </div>
+                </div>
+                <p>Промышленные прямоугольные разъемы, также известные как разъемы для тяжелых условий эксплуатации (от
+                    англ. Heavy Duty Connectors или сокращенно HDC), широко используются в строительной технике,
+                    текстильном оборудовании, упаковочном и полиграфическом оборудовании, табачном оборудовании,
+                    электровозах и нефтехимическом электрическом оборудовании, для которых требуются электрические и
+                    сигнальные соединения.</p>
+                <p>В настоящее время прямоугольные промышленные разъемы широко распространены и массово используются по
+                    всему миру. Проверенная временем конструкция и используемые материалы делают разъемы выдающимися с
+                    точки зрения электрических характеристик. Надежность электрического соединения зачастую недоступна
+                    традиционным методам соединения.</p>
+                <p>В последние 30 лет промышленные прямоугольные разъемы довольно широко распространились и на
+                    территории России. Наиболее распространенные западные бренды разъемов для тяжелых условий
+                    эксплуатации включают Amphenol, Wieland, Phoenix Contact, HARTING, Weidmüller и т. д., в настоящее
+                    время западные бренды массово покидают рынок РФ, и происходит замещение их продукции на
+                    отечественные, турецкие и китайские аналоги. Все производители прямоугольных разъемов придерживаются
+                    единых стандартных, благодаря чему, например, контактная вставка HARTING может быть легко заменена
+                    на аналогичную вставку Haoke, при этом габаритные размеры и электрические характеристики будут
+                    такими же. Незначительная разница может наблюдаться во внешних размерах корпусов.</p>
+                <h2 class="article-page__title">Преимущества сверхмощных разъемов</h2>
+                <ul class="article-page__ul-disc">
+                    <li>Промышленные разъемы позволяют осуществить предварительный монтаж большого количества сложных
+                        схем, что может значительно повысить эффективность установки оборудования и снизить количество
+                        ошибок в проводке.
+                    </li>
+                    <li>Промышленный разъем обеспечивает высокоинтегрированное соединение, а разнообразные методы
+                        комбинирования позволяют максимально эффективно использовать пространство оборудования.
+                    </li>
+                    <li>Промышленный разъем удобно и эффективно реализует модульную структуру каждого функционального
+                        блока оборудования, так что оборудование можно транспортировать, устанавливать, обслуживать и
+                        ремонтировать удобно и безопасно. Соединители широко используются в области передачи и
+                        распределения электроэнергии.
+                    </li>
+                    <li>Высокий уровень защиты (IP65, IP68), обеспечиваемый промышленными разъемами, обеспечивает
+                        эффективную защиту при работе в суровых условиях, таких как песок, дождь, холод, лед и снег, а
+                        также при загрязнении нефтью.
+                    </li>
+                </ul>
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+                <h2 class="article-page__title">Выбор промышленного прямоугольного разъема</h2>
+                <p>Чтобы выбрать прямоугольный промышленный прямоугольный разъем необходимо знать следующее:</p>
+                <ol class="article-page__ol-decimal">
+                    <li>Номинальное напряжение и номинальный ток</li>
+                    <li>Количество контактов разъема (количество цепей)</li>
+                    <li>Формат разъема (кабельный корпус, приборный корпус, корпус кабель-кабель и т.д.)</li>
+                    <li>Необходимые аксессуары (инструмент, обжимные контакты, кабельные вводы-сальники)</li>
+                </ol>
+
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+                <h2 class="article-page__title">Выбор промышленного прямоугольного разъема</h2>
+                <div class="element-container">
+                    <div class="element-column-50">
+                        <div class="element-column-wrapper">
+                            <div class="element-cell element-cell-image">
+                                <img decoding="async" width="300" height="225"
+                                    src="https://kevtek.ru/wp-content/uploads/2023/03/HE-016-M.jpg" alt="Изображение"
+                                    srcset="https://kevtek.ru/wp-content/uploads/2023/03/HE-016-M.jpg 1212w, https://kevtek.ru/wp-content/uploads/2023/03/HE-016-M-300x225.jpg 300w, https://kevtek.ru/wp-content/uploads/2023/03/HE-016-M-700x525.jpg 700w, https://kevtek.ru/wp-content/uploads/2023/03/HE-016-M-64x48.jpg 64w"
+                                    sizes="(max-width: 300px) 100vw, 300px" title="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element-column-50">
+                        <div>
+                            <div>
+                                <h5 class="article-page__h5"><a href="https://kevtek.ru/product/he-016-m/">HE-016-M</a>
+                                </h5>
+                            </div>
+                            <div class="divider">
+                                <span class="divider-separator">
+                                </span>
+                            </div>
+                            <div>
+                                <ul class="article-page__ul-none">
+                                    <li><strong>HE</strong> — серия контактных вставок — серия HE</li>
+                                    <li><strong>016</strong> — количество контактов — 16 контактов</li>
+                                    <li><strong>M</strong> — тип контактной вставки — Male (Вилка, «папа»), винтовые
+                                        клеммы</li>
+                                </ul>
+                            </div>
+                            <div class="divider">
+                                <span class="divider-separator">
+                                </span>
+                            </div>
+                            <div>
+                                <p><strong>Тип контактной вставки</strong></p>
+                                <ul class="article-page__ul-disc">
+                                    <li>M/F — вилка/розетка, винтовые клеммы</li>
+                                    <li>MC/FC — вилка/розетка, обжимные контакты</li>
+                                    <li>MS/FS — вилка/розетка, пружинные клеммы</li>
+                                    <li>MSS/FSS — вилка/розетка, пружинные клеммы (две клеммы на контакт)</li>
+                                    <li>MZ/FZ — вилка/розетка, быстрозажимные клеммы</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+
+                <h2>Расшифровка маркировки корпусов и кожухов</h2>
+
+                <div class="element-container">
+                    <div class="element-column-50">
+                        <div class="element-column-wrapper">
+                            <div class="element-cell element-cell-image">
+                                <img loading="lazy" decoding="async" max-width="75%" height="auto"
+                                    src="https://kevtek.ru/wp-content/uploads/2023/03/19300161541-300x243.jpg"
+                                    alt="Корпус промышленного прямоугольного разъема 19300161541"
+                                    srcset="https://kevtek.ru/wp-content/uploads/2023/03/19300161541-300x243.jpg 300w, https://kevtek.ru/wp-content/uploads/2023/03/19300161541-700x568.jpg 700w, https://kevtek.ru/wp-content/uploads/2023/03/19300161541-64x52.jpg 64w, https://kevtek.ru/wp-content/uploads/2023/03/19300161541.jpg 709w"
+                                    sizes="(max-width: 300px) 100vw, 300px" title="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element-column-50">
+                        <div>
+                            <div>
+                                <h5 class="article-page__h5"><a
+                                        href="https://kevtek.ru/product/h16b-se-2b-m25/">H16B-SE-2B-M25</a></h5>
+                            </div>
+                            <div class="divider">
+                                <span class="divider-separator">
+                                </span>
+                            </div>
+                            <div>
+                                <ul class="article-page__ul-none">
+                                    <li><strong>H16B</strong>&nbsp;— серия корпусов и кожухов — серия H16B —
+                                        типоразмер 16B</li>
+                                    <li><strong>SE</strong> — тип корпуса — кабельный корпус, ввод кабеля сбоку</li>
+                                    <li><strong>2B</strong> — тип фиксации — 2 фиксатора для защелки</li>
+                                    <li><strong>M25</strong> — резьба в отверстии для кабельного ввода — M25
+                                        (метрическая)
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="divider">
+                                <span class="divider-separator">
+                                </span>
+                            </div>
+                            <div>
+                                <p><strong>Тип корпуса</strong></p>
+                                <ul class="article-page__ul-disc">
+                                    <li>SE — кабельный корпус, ввод кабеля сбоку</li>
+                                    <li>SEH — кабельный корпус, ввод кабеля сбоку, высокая конструкция</li>
+                                    <li>TE — кабельный корпус, ввод кабеля сверху</li>
+                                    <li>TEH — кабельный корпус, ввод кабеля сверху, высокая конструкция</li>
+                                    <li>BK — корпус на панель, рамочный (без отверстия для кабельного ввода)</li>
+                                    <li>SF — корпус на панель (с одним или двумя отверстиями для кабельного ввода)
+                                    </li>
+                                    <li>SFH — корпус на панель, высокая конструкция (1 или 2 отверстия для кабеля)
+                                    </li>
+                                    <li>CCT — кабельный корпус (кабель-кабель) с защелкой</li>
+                                    <li>CCTH — кабельный корпус (кабель-кабель) с защелкой, высокая конструкция</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
