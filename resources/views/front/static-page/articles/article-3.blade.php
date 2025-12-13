2e5f7769e3f2ac781e664ede4b1ed43fd5f03782
johanius <info@kevtek.ru>
2025-12-13 23:06:12 +0300

    articles section on homepage

--- resources/views/front/static-page/articles/article-3.blade.php
+++ resources/views/front/static-page/articles/article-3.blade.php
@@ -298,14 +298,18 @@
                         <div class="element-column-gap">
                             <div>
                                 <img loading="lazy" decoding="async" max-height="100%" width="auto"
-                                    object-fit="cover";
-                                    src="https://kevtek.ru/wp-content/uploads/size.png"
+                                    object-fit="cover"; src="https://kevtek.ru/wp-content/uploads/size.png"
                                     class="attachment-large size-large wp-image-28607" alt="Изображение"
                                     srcset="https://kevtek.ru/wp-content/uploads/size.png 347w, https://kevtek.ru/wp-content/uploads/size-300x640.png 300w, https://kevtek.ru/wp-content/uploads/size-64x136.png 64w, https://kevtek.ru/wp-content/uploads/size-141x300.png 141w"
                                     sizes="(max-width: 347px) 100vw, 347px" title="">
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
                 </div>
 
             </div>
