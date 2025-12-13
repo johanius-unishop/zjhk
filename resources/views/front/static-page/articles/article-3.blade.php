f0ce486d71f157eef22c8af3ead525c0a070cc45
johanius <info@kevtek.ru>
2025-12-13 18:29:05 +0300

    articles section on homepage

---
+++ resources/views/front/static-page/articles/article-3.blade.php
@@ -0,0 +1,224 @@
+<main>
+    <section class="article-page">
+        <div class="article-page__container container">
+            @include('components.article-page.breadcrumbs')
+            <div class="article-page__content">
+                <h2 class="article-page__title">{{ $article->name }}</h2>
+                <h2 class="article-page__title">Совместимость компонентов разъема по типоразмеру.</h2>
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+                <p>В данной статье мы рассмотрим понятие <em>типоразмера</em> прямоугольных промышленных разъемов и его
+                    важность при подборе компонентов.</p>
+                <p>Прямоугольные промышленные разъемы имеют множество параметров, на которые следует обратить внимание
+                    для сборки нужного комплекта. Один из таких параметров — <em>типоразмер</em>, который относится как
+                    к корпусу, так и к контактной вставке.</p>
+                <p>Для правильного подбора компонентов промышленного разъема важно обеспечить их совместимость, то есть
+                    выбрать соответствующий <em>типоразмер</em>. Однако, из-за разнообразия ассортимента контактных
+                    вставок и
+                    корпусов промышленных прямоугольных разъемов, подбор совместимых компонентов может быть сложным
+                    процессом.</p>
+                <p>Каждый корпус и контактная вставка имеют параметр <em>типоразмера</em>, чтобы облегчить подбор
+                    совместимых
+                    компонентов. Несмотря на «стандартизацию» промышленных прямоугольных разъемов, разные производители
+                    могут использовать свои фирменные обозначения типоразмеров, что может вносить дополнительную
+                    путаницу. В данной статье мы представим таблицу, которая поможет разобраться в типоразмерах,
+                    используемых разными производителями промышленных прямоугольных разъемов.</p>
+                <p>Понимание <em>типоразмера</em> является ключевым фактором при подборе и установке контактных вставок
+                    в
+                    корпусы. В таблице представлены наиболее распространенные <em>типоразмеры</em> корпусов и контактных
+                    вставок.
+                </p>
+                <p>Обратите внимание на указанные <em>типоразмеры</em> и их отношение к межосевому расстоянию крепежных
+                    винтов «x — y», которые являются общими для вставок и корпусов данного <em>типоразмера</em>.</p>
+                <p>Знание <em>типоразмера</em> или умение его определить является неотъемлемой частью успешного подбора
+                    компонентов промышленных прямоугольных разъемов и обеспечивает их совместимость.</p>
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+                <h3 class="article-page__h3">Винтовые клеммы</h3>
+                <div class="element-container">
+                    <div class="element-column-30">
+                        <div class="element-column-wrapper">
+                            <div class="element-cell element-cell-image">
+                                <img decoding="async" max-width="100%" height="auto"
+                                    src="https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal.jpg"
+                                    class="attachment-large size-large wp-image-5335"
+                                    alt="Винтовые клеммы в прямоугольных промышленных разъемах"
+                                    srcset="https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal.jpg 500w, https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal-300x300.jpg 300w, https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal-100x100.jpg 100w, https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal-64x64.jpg 64w, https://kevtek.ru/wp-content/uploads/2023/04/screw_terminal-150x150.jpg 150w"
+                                    sizes="(max-width: 500px) 100vw, 500px" title="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element-column-70">
+                        <div>
+                            <div>
+                                <h5 class="article-page__h5">Преимущества</h5>
+                            </div>
+                            <div>
+                                <ul class="article-page__ul-disc">
+                                    <li>Простое и интуитивно понятное соединение</li>
+                                    <li>Монтаж можно произвести на месте без без каких-либо специальных инструментов,
+                                        достаточно иметь отверткуМонтаж можно произвести на месте без без каких-либо
+                                        специальных инструментов, достаточно иметь отвертку</li>
+                                    <li>Может использоваться для многожильных проводов клеммы</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p><strong>Винтовые клеммы</strong> являются распространенным типом клеммных соединений в промышленных
+                    прямоугольных разъемах. Они широко используются в электрических цепях для соединения проводов и
+                    кабелей. Винтовые клеммы обеспечивают надежное соединение, которое может выдерживать высокие токи и
+                    напряжения. Кроме того, они легко устанавливаются и могут быть быстро заменены при необходимости.
+                    Недостатком винтовых клемм является то, что они могут требовать больше времени и усилий для
+                    установки, чем другие типы клеммных соединений, таких как пружинные клеммы. Однако, в целом,
+                    винтовые клеммы являются надежным и простым в использовании решением для соединения проводов в
+                    электрических цепях.</p>
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+
+                <h3 class="article-page__h3">Обжимные контакты</h3>
+                <div class="element-container">
+                    <div class="element-column-30">
+                        <div class="element-column-wrapper">
+                            <div class="element-cell element-cell-image">
+                                <img loading="lazy" decoding="async" max-width="100%" height="auto"
+                                    src="https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal.jpg"
+                                    class="attachment-large size-large wp-image-5336" alt="Изображение"
+                                    srcset="https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal.jpg 500w, https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal-300x300.jpg 300w, https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal-100x100.jpg 100w, https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal-64x64.jpg 64w, https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal-150x150.jpg 150w"
+                                    sizes="(max-width: 500px) 100vw, 500px" title="">
+                                <img loading="lazy" decoding="async" max-width="100%" height="auto"
+                                    src="https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal_HE_HEE.jpg"
+                                    class="attachment-large size-large wp-image-5337" alt="Изображение"
+                                    srcset="https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal_HE_HEE.jpg 550w, https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal_HE_HEE-300x300.jpg 300w, https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal_HE_HEE-100x100.jpg 100w, https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal_HE_HEE-64x64.jpg 64w, https://kevtek.ru/wp-content/uploads/2023/04/crimp_terminal_HE_HEE-150x150.jpg 150w"
+                                    sizes="(max-width: 550px) 100vw, 550px" title="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element-column-70">
+                        <div>
+                            <div>
+                                <h5 class="article-page__h5">Преимущества</h5>
+                            </div>
+                            <div>
+                                <ul class="article-page__ul-disc">
+                                    <li>Подходят для быстрого монтажа проводки, идеальны для предварительной сборки
+                                        разъемов с большим количеством контактов</li>
+                                    <li>Используя подходящие инструменты для обжима, можно добиться стабильного качества
+                                        обжима и отличной коррозионной стойкости, почти сравнимой с холодной
+                                        сваркой.Используя подходящие инструменты для обжима, можно добиться стабильного
+                                        качества обжима и отличной коррозионной стойкости, почти сравнимой с холодной
+                                        сваркой</li>
+                                    <li>Из-за высокой плотности контактов требуется очень мало места</li>
+                                    <li>Оптимальная стоимость соединенияОптимальная стоимость соединения</li>
+                                    <li>Устойчивость к вибрации</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p>Промышленные прямоугольные разъемы с <strong>обжимными контактами</strong> широко
+                    используются в электрических цепях для соединения проводов и кабелей. Обжимные контакты обеспечивают
+                    надежное соединение, которое может выдерживать высокие токи и напряжения. Это обеспечивает
+                    стабильность работы электрической цепи. Одним из основных преимуществ обжимных контактов является их
+                    быстрый и простой монтаж. Они также обеспечивают более экономичное решение по сравнению с другими
+                    типами контактов. Кроме того, обжимные контакты обладают высокой устойчивостью к вибрации и
+                    механическим воздействиям, что делает их идеальным выбором для использования в условиях высоких
+                    нагрузок и требований к надежности.</p>
+
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+
+                <h3 class="article-page__h3">Пружинные клеммы</h3>
+                <div class="element-container">
+                    <div class="element-column-30">
+                        <div class="element-column-wrapper">
+                            <div class="element-cell element-cell-image">
+                                <img loading="lazy" decoding="async" max-width="100%" height="auto"
+                                    src="https://kevtek.ru/wp-content/uploads/2023/04/cage-clamp_terminal.jpg"
+                                    class="attachment-large size-large wp-image-5338" alt="Изображение"
+                                    srcset="https://kevtek.ru/wp-content/uploads/2023/04/cage-clamp_terminal.jpg 550w, https://kevtek.ru/wp-content/uploads/2023/04/cage-clamp_terminal-300x300.jpg 300w, https://kevtek.ru/wp-content/uploads/2023/04/cage-clamp_terminal-100x100.jpg 100w, https://kevtek.ru/wp-content/uploads/2023/04/cage-clamp_terminal-64x64.jpg 64w, https://kevtek.ru/wp-content/uploads/2023/04/cage-clamp_terminal-150x150.jpg 150w"
+                                    sizes="(max-width: 550px) 100vw, 550px" title="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element-column-70">
+                        <div>
+                            <div>
+                                <h5 class="article-page__h5">Преимущества</h5>
+                            </div>
+                            <div>
+                                <ul class="article-page__ul-disc">
+                                    <li>Этот метод подключения требует очень небольшой подготовки провода и специальных
+                                        инструментов, что приводит к низкой стоимости установки и высокой степени
+                                        механической безопасности</li>
+                                    <li>Простота подключения. Проводник и отвертка находятся в одной плоскости.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p><strong>Пружинные клеммы</strong> в промышленных прямоугольных разъемах — это удобное и надежное
+                    решение для быстрого подключения проводов. Они применяются во многих отраслях промышленности, таких
+                    как электроэнергетика, автоматизация и транспорт. Преимущества использования пружинных клемм
+                    включают простой монтаж, высокую надежность соединения и возможность быстрой замены провода без
+                    необходимости распайки. Кроме того, они обеспечивают высокую степень защиты от вибрации и
+                    механического напряжения, что делает их идеальным выбором для приложений с высокой нагрузкой и
+                    надежностью</p>
+
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+
+                <h3 class="article-page__h3">Аксиальные винтовые клеммы</h3>
+                <div class="element-container">
+                    <div class="element-column-30">
+                        <div class="element-column-wrapper">
+                            <div class="element-cell element-cell-image">
+                                <img loading="lazy" decoding="async" max-width="100%" height="auto"
+                                    src="https://kevtek.ru/wp-content/uploads/2023/04/axial_screw_terminal.jpg"
+                                    class="attachment-large size-large wp-image-5339" alt="Изображение"
+                                    srcset="https://kevtek.ru/wp-content/uploads/2023/04/axial_screw_terminal.jpg 550w, https://kevtek.ru/wp-content/uploads/2023/04/axial_screw_terminal-300x300.jpg 300w, https://kevtek.ru/wp-content/uploads/2023/04/axial_screw_terminal-100x100.jpg 100w, https://kevtek.ru/wp-content/uploads/2023/04/axial_screw_terminal-64x64.jpg 64w, https://kevtek.ru/wp-content/uploads/2023/04/axial_screw_terminal-150x150.jpg 150w"
+                                    sizes="(max-width: 550px) 100vw, 550px" title="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element-column-70">
+                        <div>
+                            <div>
+                                <h5 class="article-page__h5">Преимущества</h5>
+                            </div>
+                            <div>
+                                <ul class="article-page__ul-disc">
+                                    <li>Требуется меньше местаТребуется меньше места</li>
+                                    <li>Простота в обращении</li>
+                                    <li>Никаких специальных инструментов</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p><strong>Аксиальные винтовые клеммы</strong> — это один из типов электрических контактов, используемых
+                    в промышленных прямоугольных разъемах. Они широко применяются в приложениях, где требуется высокая
+                    надежность контакта и защита от вибрации и механических нагрузок. Клеммы легко монтируются и
+                    обеспечивают надежное соединение проводов. Однако, установка клемм может требовать больше времени и
+                    усилий, чем другие типы контактов, такие как пружинные или обжимные. В целом, аксиальные винтовые
+                    клеммы — это надежный выбор для промышленных приложений, требующих высокой надежности контакта и
+                    защиты от вибрации и механических нагрузок.</p>
+
+                <div class="divider">
+                    <span class="divider-separator">
+                    </span>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
