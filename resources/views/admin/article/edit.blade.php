2d2e42740638ef2eedce6070b947a10e1b64296d
johanius <info@kevtek.ru>
2025-12-16 21:39:09 +0300

    articles section on homepage

--- resources/views/admin/article/edit.blade.php
+++ resources/views/admin/article/edit.blade.php
@@ -16,86 +16,70 @@
 
     @include('admin.blocks.error')
 
-    <form action="{{ route('admin.article.update', $article->id) }}" method="POST">
-        @csrf
-        @method('PATCH')
-        <input type="hidden" name="id" value="{{ $article->id }}">
+    <div class="row">
+        <div class="col-12 ">
+            <div class="card card-success">
+                <div class="card-header">
+                    <h3 class="card-title">Основное</h3>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('admin.article.update', $article->id) }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        @method('PATCH')
+                        <input type="hidden" name="id" value="{{ $article->id }}">
 
-        <div class="row">
-            <div class="col-12 ">
-                <div class="card card-primary card-outline card-outline-tabs">
-                    <div class="card-header p-0 border-bottom-0">
-                        <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" id="custom-tabs-four-home-tab" data-toggle="pill"
-                                    href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home"
-                                    aria-selected="true">Основное</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="card-body">
-                        <div class="tab-content" id="custom-tabs-four-tabContent">
-                            <div class="tab-pane fade show active" id="custom-tabs-four-home" role="tabpanel"
-                                aria-labelledby="custom-tabs-four-home-tab">
-                                <div class="row">
-                                    <div class="col-lg-6 col-12">
-                                        <div class="form-group">
-                                            <label for="name">Название</label>
-                                            <input type="text" class="form-control" name="name"
-                                                value="{{ @$article->name }}">
-                                            @error('name')
-                                                <div class="alert alert-danger">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6 col-12">
-                                        <div class="form-group">
-                                            <label for="slug">ЧПУ </label>
-                                            <input type="text" class="form-control" name="slug"
-                                                value="{{ @$article->slug }}">
-                                            <div id="slugHelp" class="form-text">Заполняется автоматически. Ручное
-                                                заполнение не желательно.</div>
-                                            @error('slug')
-                                                <div class="alert alert-danger">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                </div>
+                        <div class="row">
+                            <div class="col-6">
                                 <div class="form-group">
-                                    <label for="short_description">Описание</label>
-                                    <textarea class="form-control" name="short_description" row="5" id="summernote">{{ $article->short_description }}</textarea>
+                                    <label for="name">Название статьи</label>
+                                    <input type="text" class="form-control" name="name" value="{{ $article->name }}">
+                                    @error('name')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
-                                <div class="card mt-3">
-                                    <div class="card-header">
-                                        <h3 class="card-title"><strong>Изображение статьи</strong></h3>
+                                <div class="col-lg-6 col-12">
+                                    <div class="form-group">
+                                        <label for="slug">ЧПУ </label>
+                                        <input type="text" class="form-control" name="slug"
+                                            value="{{ @$article->slug }}">
+                                        <div id="slugHelp" class="form-text">Заполняется автоматически.
+                                            Ручное заполнение не желательно.</div>
+                                        @error('slug')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
                                     </div>
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class=" col-12">
-                                                <livewire:gallery :record="$article" />
-                                            </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="form-group">
+                                            <label for="text">Краткое описание статьи</label>
+                                            <textarea class="form-control" name="text" rows="2">{{ @$article->short_description }}</textarea>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="tab-pane fade" id="custom-tabs-four-profile" role="tabpanel"
-                                aria-labelledby="custom-tabs-four-profile-tab">
-                                <livewire:seo :record="@$article">
+                                <div class="py-3 form-row justify-content-center">
+                                    <a class="btn btn-success mx-1" href="{{ route('admin.article.index') }}"
+                                        role="button">
+                                        <i class="fa fa-arrow-left"></i> К списку
+                                    </a>
+                                    <button type="submit" name="action" value="save-exit" class="btn btn-primary mx-1">
+                                        Сохранить и закрыть
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class=" py-3 form-row justify-content-center">
-                    <a class="btn btn-success" href="{{ route('admin.articles.index') }}" role="button"> <i
-                            class="fa fa-arrow-left "></i> К списку</a> &nbsp;
-                    <button type="submit" name="action" value="save" class="btn btn-primary">Сохранить</button>
-                    &nbsp;
-                    <button type="submit" name="action" value="save-exit" class="btn btn-primary">Сохранить и
-                        закрыть</button>
+                    </form>
                 </div>
             </div>
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Изображение для статьи</h3>
+                </div>
+                <livewire:article-images-gallery :record="$article" />
+            </div>
         </div>
-    </form>
+    </div>
 @stop
 
 @push('css')
