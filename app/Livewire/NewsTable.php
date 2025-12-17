a303f12cdde583edbda9a01b2e228fb2dac636ad
johanius <info@kevtek.ru>
2025-12-17 22:42:10 +0300

    news section on homepage

--- app/Livewire/NewsTable.php
+++ app/Livewire/NewsTable.php
@@ -6,14 +6,9 @@ use App\Models\News;
 use Illuminate\Database\Eloquent\Builder;
 use PowerComponents\LivewirePowerGrid\Button;
 use PowerComponents\LivewirePowerGrid\Column;
-
-use PowerComponents\LivewirePowerGrid\Facades\Filter;
-use PowerComponents\LivewirePowerGrid\Footer;
-use PowerComponents\LivewirePowerGrid\Header;
 use PowerComponents\LivewirePowerGrid\Facades\PowerGrid;
 use PowerComponents\LivewirePowerGrid\PowerGridFields;
 use PowerComponents\LivewirePowerGrid\PowerGridComponent;
-use PowerComponents\LivewirePowerGrid\Traits\WithExport;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 final class NewsTable extends PowerGridComponent
