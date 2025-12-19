00da36840042ea845e4dd8aaba8b1c6eaf5e62f6
johanius <info@kevtek.ru>
2025-12-19 13:28:53 +0300

    news section on homepage

--- resources/views/components/main-page/articles.blade.php
+++ resources/views/components/main-page/articles.blade.php
@@ -19,19 +19,16 @@
                                             $article->getFirstMedia('previewImages') &&
                                             $article->getFirstMedia('previewImages')->hasGeneratedConversion('webp-thumb'))
                                         <img src="{{ $article->getFirstMedia('previewImages')->getUrl('webp-images') }}"
-                                            alt="Новость: {{ trim($article->name) }}"
-                                            loading="lazy">
+                                            alt="Новость: {{ trim($article->name) }}" loading="lazy">
                                     @elseif (
                                         !$acceptsWebP &&
                                             $article->getFirstMedia('previewImages') &&
                                             $article->getFirstMedia('previewImages')->hasGeneratedConversion('thumb'))
                                         <img src="{{ $article->getFirstMedia('previewImages')->getUrl('thumb') }}"
-                                            alt="Новость: {{ trim($article->name) }}"
-                                            loading="lazy">
+                                            alt="Новость: {{ trim($article->name) }}" loading="lazy">
                                     @else
                                         <img src="{{ $article->getFirstMedia('previewImages') ? $article->getFirstMedia('previewImages')->getUrl() : asset('/images/default_image.jpg') }}"
-                                            alt="Новость: {{ trim($article->name) }}"
-                                            loading="lazy">
+                                            alt="Новость: {{ trim($article->name) }}" loading="lazy">
                                     @endif
                                 </div>
                                 <div>
@@ -42,69 +39,7 @@
                                 <a href="{{ route('article.show', ['slug' => $article->slug]) }}">Читать</a>
                             </div>
                         </div>
-                        <div class="swiper-slide articles__item">
-                            <div>
-                                <div>
-                                    <img src="/images/products/image-1.png" alt="статьи">
-                                </div>
-                                <div>
-                                    <h5>Типы клемм в контактных вставках</h5>
-                                    <p>Контактные вставки и cпособы присоединения проводников к контактам</p>
-                                </div>
-                                <a href="#">Читать</a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide articles__item">
-                            <div>
-                                <div>
-                                    <img src="/images/products/image-2.png" alt="статьи">
-                                </div>
-                                <div>
-                                    <h5>Подбор элементов прямоугольного промышленного разъёма
-                                    </h5>
-                                    <p>Совместимость компонентов разъёма по типоразмеру</p>
-                                </div>
-                                <a href="#">Читать</a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide articles__item">
-                            <div>
-                                <div>
-                                    <img src="/images/products/image-1.png" alt="статьи">
-                                </div>
-                                <div>
-                                    <h5>Типы клемм в контактных вставках</h5>
-                                    <p>Контактные вставки и способы присоединения проводников к контактам</p>
-                                </div>
-                                <a href="#">Читать</a>
-                            </div>
-                        </div>
-                        <div class="swiper articles__item">
-                            <div>
-                                <div>
-                                    <img src="/images/products/photo-10.png" alt="статьи">
-                                </div>
-                                <div>
-                                    <h5>Информация о сальниках (кабельных вводах)</h5>
-                                    <p>Основная информация о типах сальников и их назначении</p>
-                                </div>
-                                <a href="#">Читать</a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide articles__item">
-                            <div>
-                                <div>
-                                    <img src="/images/products/image-2.png" alt="статьи">
-                                </div>
-                                <div>
-                                    <h5>Подбор элементов прямоугольного промышленного разъёма
-                                    </h5>
-                                    <p>Совместимость компонентов разъёма по типоразмеру</p>
-                                </div>
-                                <a href="#">Читать</a>
-                            </div>
-                        </div>
-
+                    @endforeach
                 </div>
             </div>
             <div class="swiper-button-prev articles"></div>
